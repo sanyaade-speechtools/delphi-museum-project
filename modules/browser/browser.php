c3c11c4e0c3ac0c9f567ceff935c1357aef0e879
cogit <cogit@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2007-04-22 23:26:39 +0000

    Some tweaks towards initial version, but then copied to facetBrowser to include TaxoNode and Facet modules. May need attention later.

--- modules/browser/browser.php
+++ modules/browser/browser.php
@@ -1,29 +1,44 @@
 <?
-
 require_once("../../libs/env.php");
-/*
-// Query DB
-$sql = "SELECT * FROM objects o WHERE o.id = $objId LIMIT 1";
-$res =& $db->query($sql);
-if (PEAR::isError($res)) {
-    die($res->getMessage());
-}
+// Temp hack until we move from mysqli to all PEAR
+include("../facetBrowser/dbconnect.php");
+// This should go somewhere else
+require "../facetBrowser/Facet.inc";
 
-// If nothing is found...
-if ( $res->numRows() < 1 ){
-	$t->display('objectNotFound.tpl');
-	die;
-}
+	$onlyWithImgs = true;		// default to only images
+	if( !empty( $_GET['wImgs'] ) && ($_GET['wImgs'] == 'false'))
+		$onlyWithImgs = false;
 
-// Assign vars to template
-while ($row = $res->fetchRow()) {
-    $t->assign('id', $row['id']);
-}
-
-// Free the result
-$res->free();
-*/
+	$allCountsResults=$mysqli->query("SELECT n_objs_total, n_objs_w_imgs FROM DBInfo LIMIT 1");
+	if($row = $allCountsResults->fetch_array()) {
+		$t->assign("objsTotal", $row[0]);
+		$t->assign("objsTotalWImgs", $row[1]);
+	}
+	$tqCountsByCat = 
+		"select c.id, c.parent_id, c.facet_id, c.display_name, n_matches 
+		from categories c where n_matches>0 order by id";
+	$facetsResults=$mysqli->query("select id, display_name from facets");
+	GetFacetListFromResultSet($facetsResults);
+	$countsresult=$mysqli->query($tqCountsByCat);
+	PopulateFacetsFromResultSet( $countsresult, true );
+  // Facets now exist as array in $facets. Nodes are avail in hashMap.
+	$baseQ = "facetBrowse.php?";
+	$catsParam = "cats=";
+	if( !$onlyWithImgs )
+		$baseQ.= "wImgs=false&".$catsParam;
+  else 
+		$baseQ.= $catsParam;	
+	$t->assign("catsByCountQ", $tqCountsByCat);
+	foreach( $facets as $facet ) {
+		if( empty($facet->arrChildren) ) {
+			$facetTreeOutput .= "<code class=\"hidden\">Facet: "
+														.$facet->name." has no no matches</code>";
+		} else {
+			//$facet->PruneForOutput($numResultsTotal, $catIDs);
+			$facetTreeOutput .= $facet->GenerateHTMLOutput( "facet", 0, 1, $baseQ, true );
+		}
+	}
+	$t->assign("facetTree", $facetTreeOutput);
 // Display template
 $t->display('browser.tpl');
-
 ?>
