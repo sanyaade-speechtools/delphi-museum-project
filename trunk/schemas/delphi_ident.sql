f4cb4a838d65b8db4d0baef92d3a6976dc8de5ef
cogit <cogit@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2007-04-22 23:42:24 +0000

    Changed all index definitions to use 32-bit rather than 64 bit indices to improve speed on 32-bit machines (and reflecting reality of our use). Changed all tables to use MyIsam rather than InnoDB. Although we forego some consistency checking, we get improved performance. Added some additional cached values in the category table to support query-time optimizations.

--- trunk/schemas/delphi_ident.sql
+++ trunk/schemas/delphi_ident.sql
@@ -6,17 +6,18 @@ use delphi;
 --
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
-  `username` varchar(40) NOT NULL UNIQUE,
-  `passwdmd5` varchar(32) NOT NULL,				-- MD5 of the pw
-  `email` varchar(80) NOT NULL,     			-- allow for very long email addresses
-  `pending` boolean NOT NULL default true, -- on creation, is unverified
-  `blocked` boolean NOT NULL default false,
+  `id`            int(10) unsigned NOT NULL auto_increment,
+  `username`      varchar(40) NOT NULL UNIQUE,
+  `passwdmd5`     varchar(32) NOT NULL,        -- MD5 of the pw
+  `email`         varchar(80) NOT NULL,           -- allow for very long email addresses
+  `pending`       boolean NOT NULL default true, -- on creation, is unverified
+  `blocked`       boolean NOT NULL default false,
   `creation_time` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `mod_time` timestamp NOT NULL default CURRENT_TIMESTAMP 
-				on update CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `mod_time`      timestamp NOT NULL default CURRENT_TIMESTAMP 
+        on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`),
+  INDEX `user_i_username` (`username`)
+) ENGINE=MyIsam DEFAULT CHARSET=latin1;
 SHOW WARNINGS;
 
 --
@@ -24,14 +25,15 @@ SHOW WARNINGS;
 --
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
-  `name` varchar(40) NOT NULL UNIQUE,
-  `description` text,
+  `id`            int(10) unsigned NOT NULL auto_increment,
+  `name`          varchar(40) NOT NULL UNIQUE,
+  `description`   text,
   `creation_time` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `mod_time` timestamp NOT NULL default CURRENT_TIMESTAMP
-				on update CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `mod_time`      timestamp NOT NULL default CURRENT_TIMESTAMP
+        on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`),
+  INDEX `role_i_name` (`name`)
+) ENGINE=MyIsam DEFAULT CHARSET=latin1;
 SHOW WARNINGS;
 
 --
@@ -39,14 +41,15 @@ SHOW WARNINGS;
 --
 DROP TABLE IF EXISTS `permission`;
 CREATE TABLE `permission` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
-  `name` varchar(40) NOT NULL UNIQUE,
-  `description` text,
+  `id`            int(10) unsigned NOT NULL auto_increment,
+  `name`          varchar(40) NOT NULL UNIQUE,
+  `description`   text,
   `creation_time` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `mod_time` timestamp NOT NULL default CURRENT_TIMESTAMP
-				on update CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `mod_time`      timestamp NOT NULL default CURRENT_TIMESTAMP
+        on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`),
+  INDEX `perm_i_name` (`name`)
+) ENGINE=MyIsam DEFAULT CHARSET=latin1;
 SHOW WARNINGS;
 
 --
@@ -55,21 +58,21 @@ SHOW WARNINGS;
 --
 DROP TABLE IF EXISTS `user_roles`;
 CREATE TABLE `user_roles` (
-  `user_id` bigint(20) unsigned NOT NULL,
-  `role_id` bigint(20) unsigned NOT NULL,
-  `approver_id` bigint(20) unsigned default NULL,	-- Do we want to track this?
+  `user_id`       int(10) unsigned NOT NULL,
+  `role_id`       int(10) unsigned NOT NULL,
+  `approver_id`   int(10) unsigned default NULL,  -- Do we want to track this?
   `creation_time` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `mod_time` timestamp NOT NULL default CURRENT_TIMESTAMP
-				on update CURRENT_TIMESTAMP,
+  `mod_time`      timestamp NOT NULL default CURRENT_TIMESTAMP
+        on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`user_id`, `role_id`),
-  -- Do we need to index the roles? Will we ask "who all has role X"?
   CONSTRAINT `ur_ibfk_1` FOREIGN KEY (`user_id`)
-			  REFERENCES `user` (`id`),
+        REFERENCES `user` (`id`),
   CONSTRAINT `ur_ibfk_2` FOREIGN KEY (`role_id`)
-			  REFERENCES `role` (`id`),
+        REFERENCES `role` (`id`),
   CONSTRAINT `ur_ibfk_3` FOREIGN KEY (`approver_id`)
-			  REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+        REFERENCES `user` (`id`)
+  -- Do we need to index the roles? Will we ask "who all has role X"?
+) ENGINE=MyIsam DEFAULT CHARSET=latin1;
 SHOW WARNINGS;
 
 --
@@ -80,13 +83,13 @@ SHOW WARNINGS;
 --
 DROP TABLE IF EXISTS `role_perms`;
 CREATE TABLE `role_perms` (
-  `role_id` bigint(20) unsigned NOT NULL,
-  `perm_id` bigint(20) unsigned NOT NULL,
+  `role_id`       int(10) unsigned NOT NULL,
+  `perm_id`       int(10) unsigned NOT NULL,
   `creation_time` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `mod_time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `mod_time`      timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   UNIQUE KEY  (`role_id`,`perm_id`),
-  -- Do we need to index the roles? Will we ask "who all has role X"?
   CONSTRAINT `rp_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
   CONSTRAINT `rp_ibfk_2` FOREIGN KEY (`perm_id`) REFERENCES `permission` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  -- Do we need to index the roles? Will we ask "who all has role X"?
+) ENGINE=MyIsam DEFAULT CHARSET=latin1;
 SHOW WARNINGS;
--- trunk/schemas/mainObjInfo.sql
+++ trunk/schemas/mainObjInfo.sql
@@ -10,7 +10,7 @@ USE delphi;
 -- The DBInfo table has a single row and is just used to hold system-wide
 -- parameters such as the sizes of alternate image sizes, the version of this
 -- DB schema, etc.
-DROP TABLE IF EXISTS DBInfo;
+DROP TABLE IF EXISTS DBInfo \p;
 CREATE TABLE DBInfo (
   `version`           VARCHAR(16) NOT NULL,
   -- TFacetMaskWidth must agree with the FacetMaskCache definition
@@ -26,61 +26,64 @@ CREATE TABLE DBInfo (
   `thumb_basepath`    VARCHAR(255) NULL,    
   `medium_basepath`   VARCHAR(255) NULL,    
   `large_basepath`    VARCHAR(255) NULL,    
-  `creation_time` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `mod_time` timestamp NOT NULL default CURRENT_TIMESTAMP 
+  `creation_time`     timestamp NOT NULL default '0000-00-00 00:00:00',
+  `mod_time`          timestamp NOT NULL default CURRENT_TIMESTAMP 
         on update CURRENT_TIMESTAMP
-);
+); 
 SHOW WARNINGS;
 
 INSERT INTO DBInfo( version, creation_time ) 
-  VALUES( '0.1 alpha', now() );
+  VALUES( '0.2 alpha', now() ) \p;
 SHOW WARNINGS;
 
 -- Define the main object table
-DROP TABLE IF EXISTS `objects`;
+DROP TABLE IF EXISTS `objects` \p;
 CREATE TABLE `objects` (
-  `id`            BIGINT(10) PRIMARY KEY NOT NULL,
+  `id`            INT(10) UNSIGNED PRIMARY KEY NOT NULL,
   `objnum`        VARCHAR(80) NOT NULL,
   `name`          VARCHAR(255) NOT NULL,
   `description`   text NULL,
   -- All paths are relative to the configured image roots`
-  `img_path`  VARCHAR(255) NULL,    
+  `img_path`      VARCHAR(255) NULL,    
   `creation_time` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `mod_time` timestamp NOT NULL default CURRENT_TIMESTAMP 
+  `mod_time`      timestamp NOT NULL default CURRENT_TIMESTAMP 
         on update CURRENT_TIMESTAMP,
   INDEX `obj_id_index` (`id`)
-)ENGINE=InnoDB;
+)ENGINE=MyIsam;
 SHOW WARNINGS;
 
 --Sets allow users (or the system) to group together items for
 -- a particular reason (or simply as favorites).
-DROP TABLE IF EXISTS `sets`;
+DROP TABLE IF EXISTS `sets` \p;
 CREATE TABLE `sets` (
-  `id` bigint(20) PRIMARY KEY NOT NULL auto_increment,
-  `name` VARCHAR(255) NOT NULL,
-  `description` text NULL,
-  `policy` enum('public', 'private'), -- later: friends, etc.
-  `owner_id` bigint(20) NOT NULL,
-  `creation_time` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `mod_time` timestamp NOT NULL default CURRENT_TIMESTAMP 
+  `id`             int(10) unsigned PRIMARY KEY NOT NULL auto_increment,
+  `name`           VARCHAR(255) NOT NULL,
+  `description`    text NULL,
+  `policy`         enum('public', 'private'), -- later: friends, etc.
+  `owner_id`       int(10) unsigned NOT NULL,
+  `creation_time`  timestamp NOT NULL default '0000-00-00 00:00:00',
+  `mod_time`       timestamp NOT NULL default CURRENT_TIMESTAMP 
         on update CURRENT_TIMESTAMP,
   INDEX `sets_id_index` (`id`),
+  INDEX `sets_id_owner` (`owner_id`),
   CONSTRAINT `se_ibfk_1` FOREIGN KEY (`owner_id`)
-      REFERENCES `user` (`id`) ON DELETE CASCADE
-)ENGINE=InnoDB;
+      REFERENCES `user` (`id`)
+)ENGINE=MyIsam;
 SHOW WARNINGS;
 
-DROP TABLE IF EXISTS `set_objs`;
+DROP TABLE IF EXISTS `set_objs` \p;
 CREATE TABLE `set_objs` (
-  `set_id` bigint(20) NOT NULL,
-  `obj_id` bigint(20) NOT NULL,
+  `set_id`   int(10) unsigned NOT NULL,
+  `obj_id`   int(10) unsigned NOT NULL,
   INDEX `so_set_index` (`set_id`),
   INDEX `so_obj_index` (`obj_id`),
+	-- Ensure we have no dupes in a given set
+  UNIQUE INDEX `so_so_index` (`set_id`,`obj_id`),
   CONSTRAINT `seo_ibfk_1` FOREIGN KEY (`set_id`)
-      REFERENCES `sets` (`id`) ON DELETE CASCADE,
+      REFERENCES `sets` (`id`),
   CONSTRAINT `seo_ibfk_2` FOREIGN KEY (`obj_id`)
-      REFERENCES `objects` (`id`) ON DELETE CASCADE
-)ENGINE=InnoDB;
+      REFERENCES `objects` (`id`)
+)ENGINE=MyIsam;
 SHOW WARNINGS;
 
 /*
@@ -92,18 +95,18 @@ SHOW WARNINGS;
  * condition is met, and then the category acts as a boolean attribute.
  * TODO add creation and modification times.
  */
-DROP TABLE IF EXISTS `categories`;
+DROP TABLE IF EXISTS `categories` \p;
 CREATE TABLE `categories` (
-  `id`        BIGINT(10) PRIMARY KEY NOT NULL auto_increment,
+  `id`              INT(10) UNSIGNED PRIMARY KEY NOT NULL auto_increment,
   -- May not have a parent if facet root ; otherwise parent is non-null and >0.
-  `parent_id` BIGINT(10) NULL,
+  `parent_id`       INT(10) UNSIGNED NULL,
   -- Note that the names may not be unique, since they can appear in multiple
   -- facets and even in multiple places in one facet.
-  `name`          VARCHAR(255) NOT NULL,
-  `display_name`  VARCHAR(255) NOT NULL,
-  `facet_id`  BIGINT(10) NULL,
+  `name`            VARCHAR(255) NOT NULL,
+  `display_name`    VARCHAR(255) NOT NULL,
+  `facet_id`        INT(10) UNSIGNED NULL,
   -- Whether children are exclusive
-  `select_mode` ENUM ('single', 'multiple' ) NOT NULL DEFAULT 'multiple',
+  `select_mode`     ENUM ('single', 'multiple' ) NOT NULL DEFAULT 'multiple',
   /* This portion must be created algorithmically by the
    * mechanism that denormalizes the tables into masks.
   -- If total nodes in a facet is > ( 255 * maskwidth) then MaskIndexBase becomes a MEDIUMINT
@@ -111,47 +114,48 @@ CREATE TABLE `categories` (
   NMasks TINYINT(3) UNSIGNED NOT NULL DEFAULT 1, -- which mask segment is this in?
   MaskBit TINYINT(2) UNSIGNED NOT NULL,    -- which bit in the mask is this?
  */
-  `always_inferred` BIT(1) NULL,
-  `n_matches` int(10) NOT NULL default 0,
+  `always_inferred` TINYINT(1) NOT NULL default 0,
+  `n_matches`       int(10) NOT NULL default 0,
+  `n_matches_w_img` int(10) NOT NULL default 0,
   INDEX `cat_id_index` (`id`),
   INDEX `cat_name_index` (`name`),    -- Removed until need clarified
   CONSTRAINT `cat_ibfk_1` FOREIGN KEY (`parent_id`)
-    REFERENCES `categories` (`id`) ON DELETE CASCADE,
+    REFERENCES `categories` (`id`),
   CONSTRAINT `cat_ibfk_2` FOREIGN KEY (`facet_id`)
-      REFERENCES `facets` (`id`) ON DELETE CASCADE
-)ENGINE=InnoDB;
+      REFERENCES `facets` (`id`)
+)ENGINE=MyIsam;
 SHOW WARNINGS;
 
 /*
  * Provides all the hooks (matching tokens) for Categories.
  * TODO add creation and modification times.
  */
-DROP TABLE IF EXISTS `hooks`;
+DROP TABLE IF EXISTS `hooks` \p;
 CREATE TABLE `hooks` (
-  `id`        BIGINT(10) PRIMARY KEY NOT NULL auto_increment,
-  `cat_id` BIGINT(10) NULL,
-  `token`      VARCHAR(255) NOT NULL,
+  `id`       INT(10) UNSIGNED PRIMARY KEY NOT NULL auto_increment,
+  `cat_id`   INT(10) UNSIGNED NULL,
+  `token`    VARCHAR(255) NOT NULL,
   INDEX `hk_cat_index` (`cat_id`),
   INDEX `hk_token_index` (`token`),
   CONSTRAINT `hk_ibfk_1` FOREIGN KEY (`cat_id`)
-      REFERENCES `categories` (`id`) ON DELETE CASCADE
-)ENGINE=InnoDB;
+      REFERENCES `categories` (`id`)
+)ENGINE=MyIsam;
 SHOW WARNINGS;
 
 /*
  * Provides all the exclusions (countraindications) for Categories.
  * TODO add creation and modification times.
  */
-DROP TABLE IF EXISTS `exclusions`;
+DROP TABLE IF EXISTS `exclusions` \p;
 CREATE TABLE `exclusions` (
-  `id`        BIGINT(10) PRIMARY KEY NOT NULL auto_increment,
-  `cat_id` BIGINT(10) NULL,
-  `token`      VARCHAR(255) NOT NULL,
+  `id`       INT(10) UNSIGNED PRIMARY KEY NOT NULL auto_increment,
+  `cat_id`   INT(10) UNSIGNED NULL,
+  `token`    VARCHAR(255) NOT NULL,
   INDEX `ex_cat_index` (`cat_id`),
   INDEX `ex_token_index` (`token`),
   CONSTRAINT `ex_ibfk_1` FOREIGN KEY (`cat_id`)
-      REFERENCES `categories` (`id`) ON DELETE CASCADE
-)ENGINE=InnoDB;
+      REFERENCES `categories` (`id`)
+)ENGINE=MyIsam;
 SHOW WARNINGS;
 
 /*
@@ -167,24 +171,24 @@ SHOW WARNINGS;
  * imported at index time. The column sizes for the maskIndex and maskSize
  * must match the optimized definitions in the FacetCache table.
  */
-DROP TABLE IF EXISTS `facets`;
+DROP TABLE IF EXISTS `facets` \p;
 CREATE TABLE `facets` (
-  `id`             BIGINT(10) PRIMARY KEY NOT NULL auto_increment,
+  `id`             INT(10) UNSIGNED PRIMARY KEY NOT NULL auto_increment,
   `name`           VARCHAR(255) NOT NULL,
 -- In UI, if not filtering. E.g.: "All People"
   `display_name`   VARCHAR(255) NOT NULL,
-  `num_categories` BIGINT(5) UNSIGNED NULL,
+  `num_categories` INT(10) UNSIGNED UNSIGNED NULL,
   `num_masks`      TINYINT(3) UNSIGNED NULL
 --  `root_cat_id`  INT(10) UNSIGNED NULL,
 --  CONSTRAINT `fcat_ibfk_1` FOREIGN KEY (`root_cat_id`)
---      REFERENCES `categories` (`id`) ON DELETE CASCADE
-)ENGINE=InnoDB;
+--      REFERENCES `categories` (`id`)
+)ENGINE=MyIsam;
 SHOW WARNINGS;
 
 /*
  * Facet Masks Cache all the categories from a facet that are associated to
  * a given image, as a sparse list of bitmasks. This is fast and space efficient.
- * If we a facet has 200 categories and we have chosen a mask size of 32 (BIGINT)
+ * If a facet has 200 categories and we have chosen a mask size of 32 (INT)
  * then there will be 8 potential masks for each image. In practice, each image
  * will tend to have only a few masks at most, and so we get caching with little cost.
  * Each category knows which mask segment it is in, and which bit in that segment.
@@ -211,18 +215,19 @@ CREATE TABLE NewPhotoDB.FacetCaches (
  * Provides associations of categorizations of objects.
  * TODO add creation and modification times.
  */
-DROP TABLE IF EXISTS `obj_cats`;
+DROP TABLE IF EXISTS `obj_cats` \p;
 CREATE TABLE `obj_cats` (
-  `obj_id`      bigint(20) NOT NULL,
-  `cat_id`      bigint(20) NOT NULL,
-  `inferred`    BIT(1) NOT NULL DEFAULT 0,
+  `obj_id`      int(10) unsigned NOT NULL,
+  `cat_id`      int(10) unsigned NOT NULL,
+  `inferred`    TINYINT(1) NOT NULL DEFAULT 0,
   `reliability` TINYINT(1) NOT NULL DEFAULT 9,
   INDEX `oc_obj_index` (`obj_id`),
   INDEX `oc_cat_index` (`cat_id`),
+  UNIQUE INDEX `oc_obj_cat_index` (`obj_id`,`cat_id`),
   CONSTRAINT `obc_ibfk_1` FOREIGN KEY (`obj_id`)
-      REFERENCES `objects` (`id`) ON DELETE CASCADE,
+      REFERENCES `objects` (`id`),
   CONSTRAINT `obc_ibfk_2` FOREIGN KEY (`cat_id`)
-      REFERENCES `categories` (`id`) ON DELETE CASCADE
-)ENGINE=InnoDB;
+      REFERENCES `categories` (`id`)
+)ENGINE=MyIsam;
 SHOW WARNINGS;
 
