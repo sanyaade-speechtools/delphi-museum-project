9c19d2390edc52c157e1e79e42162891b2a20e6a
cogit <cogit@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2007-04-22 23:37:56 +0000

    Many changes to improve performance of the queries, and to take advantage of an improved schema. Now orders the categories in the query in decreasing order of category associations (across the whole collection). This minimizes the size of scans required on nested select queries, which has a major speed benefit. Also moved the join on objects to within the next-to top select to ensure MySQL uses an index for the join.

--- trunk/modules/facetBrowser/facetBrowse.php
+++ trunk/modules/facetBrowser/facetBrowse.php
@@ -14,13 +14,13 @@ function buildMainQueryForTerm( $catIDList, $iID ) {
 		$qName .= (string)$iID;
 	}
 	if( $iID == count($catIDList)-1 ) {		// simple form
-		return "(select distinct obj_id from obj_cats where cat_id=".(string)$catIDList[$iID].") ".$qName;
+		return "(SELECT obj_id from obj_cats where cat_id=".(string)$catIDList[$iID].") ".$qName;
 	}
 	// Not the last one, so we build the return string as a join on this and the recursively
 	// computed string
 	$subQ = buildMainQueryForTerm( $catIDList, $iID+1 );
 	$subName = "sub".($iID+1);
-	return "(select oc.obj_id from obj_cats oc,".$subQ."
+	return "(SELECT oc.obj_id from obj_cats oc,".$subQ."
 					 where oc.obj_id=".$subName.".obj_id and oc.cat_id="
 						.(string)$catIDList[$iID].") ".$qName;
 }
@@ -54,13 +54,35 @@ function buildStringForQueryTerms( $catIDs ) {
 		echo "<h2>No query params specified</h2>";
 		return;
 	} else {
-		$cats = $_GET['cats'];
-		$catIDs = explode( ",", $cats );
-
-		$tqMain = buildMainQueryForTerm( $catIDs, 0 );
 		$onlyWithImgs = true;		// default to only images
 		if( !empty( $_GET['wImgs'] ) && ($_GET['wImgs'] == 'false'))
 			$onlyWithImgs = false;
+		$cats = $_GET['cats'];
+		$catIDs = explode( ",", $cats );
+		if( count($catIDs) == 1 ) {
+			$t->assign("catsByCountQ", "Unneeded");
+		} else {
+			// We need to order the cats to put the ones with the most objects at the outside
+			$tqCatOrder = "SELECT id, ".($onlyWithImgs?"n_matches_w_img":"n_matches").
+										" n from categories where ";
+			$fFirst = true;
+			foreach( $catIDs as $catID ) {
+				if( $fFirst )
+					$fFirst = false;
+				else
+					$tqCatOrder .= " or ";
+				$tqCatOrder .= "id=".$catID;
+			}
+			$tqCatOrder .= " order by n desc";
+			$catsQResult=$mysqli->query($tqCatOrder);
+			$t->assign("tqCatOrder", $tqCatOrder);
+			unset($catIDs);
+			while($row = $catsQResult->fetch_array()) {
+				$catIDs[] = $row[0];
+			}
+		}
+
+		$tqMain = buildMainQueryForTerm( $catIDs, 0 );
 		$pageNum = 0;
 		if( !empty( $_GET['page'] ))
 			$pageNum = 1*$_GET['page'];
@@ -70,32 +92,27 @@ function buildStringForQueryTerms( $catIDs ) {
 		if( $onlyWithImgs ) {
 			$qual = " (with images)";
 			$tqCountsByCat =
-				"select c.id, c.parent_id, c.facet_id, c.display_name, count(*) from categories c,
-					objects o, (select distinct oc.obj_id, oc.cat_id from obj_cats oc,
+				"SELECT c.id, c.parent_id, c.facet_id, c.display_name, count(*) FROM categories c,
+					(SELECT oc.obj_id, oc.cat_id from obj_cats oc, objects o, 
 				".$tqMain."
-				where oc.obj_id=tqMain.obj_id) tqTop
-				where c.id=tqTop.cat_id and o.id=tqTop.obj_id and NOT o.img_path IS NULL
-			 	group by c.id order by c.id";
+				WHERE oc.obj_id=tqMain.obj_id AND o.id=tqMain.obj_id AND NOT o.img_path IS NULL) tqTop
+				WHERE c.id=tqTop.cat_id GROUP BY c.id ORDER BY c.id";
 			$tqFull =
-			"select o.id, o.objnum, o.name, o.description, o.img_path
-			 from objects o,".$tqMain." where o.id=tqMain.obj_id and NOT o.img_path IS NULL limit ".$_DELPHI_PAGE_SIZE;
-			$tqFullCount =
-				"select count(*) from objects o,".$tqMain."
-				where o.id=tqMain.obj_id and NOT o.img_path IS NULL";
+			"SELECT SQL_CALC_FOUND_ROWS o.id, o.objnum, o.name, o.description, o.img_path
+			 FROM objects o,".$tqMain." WHERE o.id=tqMain.obj_id AND NOT o.img_path IS NULL LIMIT ".$_DELPHI_PAGE_SIZE;
 		} else {
 			$qual = "";
 			$tqCountsByCat =
-				"select c.id, c.parent_id, c.facet_id, c.display_name, count(*) from categories c,
-				(select distinct oc.obj_id, oc.cat_id from obj_cats oc,
+				"SELECT c.id, c.parent_id, c.facet_id, c.display_name, count(*) from categories c,
+				(SELECT oc.obj_id, oc.cat_id from obj_cats oc,
 				".$tqMain."
 				where oc.obj_id=tqMain.obj_id) tqTop
 				 where c.id=tqTop.cat_id group by c.id order by c.id";
 			$tqFull =
-			"select o.id, o.objnum, o.name, o.description, o.img_path
+			"SELECT SQL_CALC_FOUND_ROWS o.id, o.objnum, o.name, o.description, o.img_path
 			 from objects o,".$tqMain." where o.id=tqMain.obj_id limit ".$_DELPHI_PAGE_SIZE;
-			$tqFullCount =
-				"select count(*) from objects o,".$tqMain." where o.id=tqMain.obj_id";
 		}
+		$tqFullCount = "SELECT FOUND_ROWS()";
 		if( $pageNum > 0 )
 			$tqFull .= " OFFSET ".($_DELPHI_PAGE_SIZE*$pageNum);
 		$t->assign("catsByCountQ", $tqCountsByCat);
@@ -105,8 +122,6 @@ function buildStringForQueryTerms( $catIDs ) {
 
 		$objsresult=$mysqli->query($tqFull);
 		$fullCountResult=$mysqli->query($tqFullCount);
-		// This is unreliable because of the LIMIT clause in the query.
-		// $numResultsTotal = $mysqli->num_rows($objsresult);
 		if($row = $fullCountResult->fetch_array()) {
 			$numResultsTotal = $row[0];
 			$numPagesTotal = ceil($numResultsTotal/$_DELPHI_PAGE_SIZE);
@@ -121,7 +136,7 @@ function buildStringForQueryTerms( $catIDs ) {
 			$pageNums[] = 1;
 		}
 	}
-	$facetsResults=$mysqli->query("select id, display_name from facets");
+	$facetsResults=$mysqli->query("SELECT id, display_name from facets order by id");
 	GetFacetListFromResultSet($facetsResults);
 	$countsresult=$mysqli->query($tqCountsByCat);
 	PopulateFacetsFromResultSet( $countsresult, true );
@@ -150,9 +165,10 @@ function buildStringForQueryTerms( $catIDs ) {
 	$facetTreeOutput = "";
 
 	foreach( $facets as $facet ) {
-		if( empty($facet->arrChildren) )
-			echo( "<code style=\"display:none;\">Facet: ".$facet->name." has no no matches</code>" );
-		else {
+		if( empty($facet->arrChildren) ) {
+			$facetTreeOutput .= "<code class=\"hidden\">Facet: "
+														.$facet->name." has no no matches</code>";
+		} else {
 			$facet->PruneForOutput($numResultsTotal, $catIDs);
 			$facetTreeOutput .= $facet->GenerateHTMLOutput( "facet", 0, 1, $baseQ, false );
 		}
