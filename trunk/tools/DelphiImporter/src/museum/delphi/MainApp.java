5cb379bf24eba7c0ef1d09f93595c41b73766163
cogit <cogit@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2007-04-23 00:11:09 +0000

    Changed the way we do categorization so that we can gather up all the categories for a given object (i.e., across all columns) before we emit any output, so that we can eliminate duplicates and use the highest relevance match. Added secondary model for output of object-category associations, supporting the mysqlload model as well as INSERT-VALUE statements. The load model is much faster and slightly more terse. Currently defaults to use load rather than INSERT output.

--- trunk/tools/DelphiImporter/src/museum/delphi/MainApp.java
+++ trunk/tools/DelphiImporter/src/museum/delphi/MainApp.java
@@ -5,6 +5,7 @@ package museum.delphi;
 
 //import java.util.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.BorderLayout;
@@ -707,7 +708,7 @@ public class MainApp {
 	}
 
 
-	private int dumpSQLForObjectCat( int id, TaxoNode category, boolean inferred, int reliability,
+	private int dumpSQLInsertForObjectCat( int id, TaxoNode category, boolean inferred, int reliability,
 			BufferedWriter writer, boolean fFirst, boolean fWithNewlines ) {
 		String entry = "";
 		try {
@@ -718,6 +719,27 @@ public class MainApp {
 			}
 			// obj_id, cat_id, inferred, reliability
 			entry = "("+id+","+category.id+(inferred?",1,":",0,")+reliability+")";
+			writer.append(entry);
+		} catch( IOException e ) {
+            e.printStackTrace();
+			throw new RuntimeException("Problem writing obj_cats entry: "+entry );
+		} catch( RuntimeException e ) {
+			JOptionPane.showMessageDialog(getJFrame(), "Error encountered:\n" + e.toString(),
+								"Categorization SQL Dump File Error", JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+		}
+		// When we add the work to add the inferred cats, this will return non-zero
+		return 1;
+	}
+
+	private int dumpSQLLoadFileForObjectCat( int id, TaxoNode category, boolean inferred, int reliability,
+			BufferedWriter writer, boolean fFirst, boolean fWithNewlines ) {
+		String entry = "";
+		String sep = "|";
+		String newLn = "\n";
+		try {
+			// obj_id, cat_id, inferred, reliability
+			entry = id+sep+category.id+sep+(inferred?"1":"0")+sep+reliability+newLn;
 			writer.append(entry);
 		} catch( IOException e ) {
             e.printStackTrace();
@@ -1366,6 +1388,8 @@ public class MainApp {
 					// Set up the correct DB
 					 writer.append("USE delphi;");
 					 writer.newLine();
+					 writer.append("truncate facets;");
+					 writer.newLine();
 					// First, let's populate the facets table
 					 writer.append("INSERT INTO facets( id, name, display_name ) VALUES" );
 					 boolean fFirst = true;
@@ -1380,13 +1404,16 @@ public class MainApp {
 					}
 					writer.append(';');
 					writer.newLine();
+					 writer.append("truncate categories;");
+					 writer.newLine();
 					// Now, let's populate the categories table
-					writer.append("INSERT INTO categories() VALUES" );
+					writer.append("INSERT INTO categories(id, parent_id, name, display_name, facet_id, select_mode, always_inferred) VALUES" );
 					writer.newLine();
 					fFirst = true;
 					for( Facet facet : facetMapHashTree.GetFacets() ) {
 						for( TaxoNode child : facet.children ) {
-							dumpSQLForVocabNode( child, writer, fFirst, fWithNewlines );
+							// for each child of the facet, dump. Mark as a root in facet.
+							dumpSQLForVocabNode( child, writer, true, fFirst, fWithNewlines );
 							fFirst = false;
 						}
 					}
@@ -1410,24 +1437,29 @@ public class MainApp {
 
 	// We insert all the fields in declaration order:
 	// id, parent_id, name, display_name, facet_id, select_mode, always_inferred
+	// Since the facet roots are TaxoNodes, the effective roots in all facets
+	// (i.e., the first level nodes, all will have non-null parents).
+	// We pass in a flag to simplify this (rather than checking if parent->parent is null).
 	// TODO Need to think about getting synonyms into DB to support
 	//  UI that lets people search for categories.
-	// What abt token-based pattern model?
-	private void dumpSQLForVocabNode( TaxoNode node, BufferedWriter writer, boolean fFirst, boolean fWithNewlines ) {
+	// What about token-based pattern model?
+	private void dumpSQLForVocabNode( TaxoNode node, BufferedWriter writer,
+			boolean fRootNode, boolean fFirst, boolean fWithNewlines ) {
 		try {
 			if( !fFirst ) {
 				writer.append(',');
 				if( fWithNewlines )
 					writer.newLine();
 			}
-			int parentID = (node.parent == null)?-1:node.parent.id;
+			String parentID = (fRootNode || node.parent == null)?"null":Integer.toString(node.parent.id);
 			String select = (node.selectSingle)? "'single'":"'multiple'";
 			int infer = (node.inferredByChildren)? 1:0;
 			writer.append("("+node.id+","+parentID+",\""+node.name+"\",\""+node.displayName+"\","
 							+node.facetid+","+select+","+infer+")");
 			if( node.children != null )
 				for( TaxoNode child : node.children ) {
-					dumpSQLForVocabNode( child, writer, false, fWithNewlines );
+					// Recusrse for children. Note cannot be roots, nor first.
+					dumpSQLForVocabNode( child, writer, false, false, fWithNewlines );
 				}
 		} catch( IOException e ) {
             e.printStackTrace();
@@ -1504,13 +1536,16 @@ public class MainApp {
     	String basefilename = null;
     	String extension = ".sql";
     	String currFilename = null;
+    	boolean fDumpAsSQLInsert = false;
     	int iOutputFile;
     	int nObjCatsOutMax = Integer.MAX_VALUE;
+    	int nObjCatsReport = 100000;
+    	int nObjCatsTillReport;
     	// Each line in the output file is about 20 chars max.
     	// We want a max of 5 MB files, so we can take up to
     	// 5000000 / 20 = 250000.
     	// To be safer, we'll set it at 200K
-    	int nObjsCatsPerDumpFile = 200000;
+    	int nObjsCatsPerDumpFile = Integer.MAX_VALUE;
     	int nObjCatsDumpedToFile = 0;
     	int nObjCatsDumpedTotal = 0;
     	// Hold the information for the Facet(s)
@@ -1526,10 +1561,12 @@ public class MainApp {
 				skipCol[iCol] = ( colDumpInfo[iCol].columnReliabilityForFacet(facetName) == 0);
 			}
 			filename = metaDataReader.getFileName();
-	    	int iDot = filename.lastIndexOf('.');
-	    	if( iDot<=0 )
-	    		throw new RuntimeException("categorizeForFacet: odd input filename!");
-	    	basefilename = filename.substring(0, iDot)+"_"+facetName+"_cats_";
+			// Find last slash or backslash (to allow for windows paths), to get basepath
+	    	int iSlash = Math.max(filename.lastIndexOf('/'), filename.lastIndexOf('\\'));
+	    	//int iDot = filename.lastIndexOf('.');
+	    	if( iSlash<=0 )
+	    		throw new RuntimeException("categorizeForFacet: odd input filename:\n"+filename);
+	    	basefilename = filename.substring(0, iSlash+1)+"obj_cats."+facetName+"_";
 			BufferedWriter objCatsWriter = null;
 	    	iOutputFile = 0;
 			ArrayList<String> nextLine;
@@ -1537,6 +1574,8 @@ public class MainApp {
 			boolean fWithNewlines = true;
 			int lastIDVal = -1;
 			ArrayList<String> lastStrings = new ArrayList<String>();
+			HashMap<TaxoNode, Float> matchedCats = new HashMap<TaxoNode, Float>();
+			nObjCatsTillReport = nObjCatsReport;
 			while((nextLine = metaDataReader.getNextLineAsColumns()) != null ) {
 				if(nextLine.get(0).length() == 0) {
 					debug(2,"Skipping line with empty id" );
@@ -1577,31 +1616,38 @@ public class MainApp {
 					fFirst = true;
 		    		currFilename = basefilename + iOutputFile + extension;
 		    		objCatsWriter = new BufferedWriter(new FileWriter( currFilename ));
-		    		objCatsWriter.append("USE delphi;");
-		    		objCatsWriter.newLine();
-		    		objCatsWriter.append("INSERT IGNORE INTO obj_cats(obj_id, cat_id, inferred, reliability) VALUES" );
-		    		objCatsWriter.newLine();
+		    		if( fDumpAsSQLInsert ) {
+			    		objCatsWriter.append("USE delphi;");
+			    		objCatsWriter.newLine();
+			    		objCatsWriter.append("INSERT IGNORE INTO obj_cats(obj_id, cat_id, inferred, reliability) VALUES" );
+			    		objCatsWriter.newLine();
+		    		}
 				}
 				// Otherwise, now we output the info for the last Line, and then transfer
 				// the next line to Last line and loop
-				// We consider each facet in turn
+				// We need to track the matches we have made, and the reliability assigned per column.
+				// If two columns generate the same category, then we use the higher reliability.
+				matchedCats.clear();
 				for(int i=1; i<nCols; i++ ) {
 					if(skipCol[i])				// Do not bother with misc cols
 						continue;
 					String source = lastStrings.get(i);
 					if( source.length() <= 1 )
 						continue;
-					int nCatsDumped = categorizeAndDumpSQLForObjectAndFacet( lastIDVal, source,
-												facetName, colDumpInfo[i],
-												objCatsWriter, fFirst, fWithNewlines );
-					if( nCatsDumped > 0) {
-						nObjCatsDumpedTotal += nCatsDumped;
-						nObjCatsDumpedToFile += nCatsDumped;
-						fFirst = false;
-					}
+					categorizeObjectForFacet( lastIDVal, source,
+							facetName, colDumpInfo[i], matchedCats );
+				}
+				int nCatsDumped = dumpSQLForObjCatsOnFacet( lastIDVal, matchedCats,
+										objCatsWriter, fFirst, fWithNewlines, fDumpAsSQLInsert );
+				if( nCatsDumped > 0) {
+					nObjCatsDumpedTotal += nCatsDumped;
+					nObjCatsDumpedToFile += nCatsDumped;
+					nObjCatsTillReport -= nCatsDumped;
+					fFirst = false;
 				}
 				lastIDVal = id;
 				lastStrings = nextLine;
+				matchedCats.clear();
 				if( nObjCatsDumpedTotal >= nObjCatsOutMax ) {
 					if(lastStrings.size() > 0) {
 						for(int i=1; i<nCols; i++ ) {
@@ -1610,30 +1656,37 @@ public class MainApp {
 							String source = lastStrings.get(i);
 							if( source.length() <= 1 )
 								continue;
-							int nCatsDumped = categorizeAndDumpSQLForObjectAndFacet( lastIDVal, source,
-									facetName, colDumpInfo[i],
-									objCatsWriter, fFirst, fWithNewlines );
-							if( nCatsDumped > 0) {
-								// Gratuitous since have already decided to quit
-								// nObjsProcessedTotal += nCatsFound;
-								fFirst = false;
-							}
+							categorizeObjectForFacet( lastIDVal, source,
+									facetName, colDumpInfo[i], matchedCats );
 						}
 					}
 					break;
 				}
+				nCatsDumped = dumpSQLForObjCatsOnFacet( lastIDVal, matchedCats,
+						objCatsWriter, fFirst, fWithNewlines, fDumpAsSQLInsert );
+				if( nCatsDumped > 0) {
+					// Gratuitous since have already decided to quit
+					// nObjsProcessedTotal += nCatsFound;
+					fFirst = false;
+				}
 				if( nObjCatsDumpedToFile >= nObjsCatsPerDumpFile ) {
-					objCatsWriter.append(";");
+					if( fDumpAsSQLInsert )
+						objCatsWriter.append(";");
 					objCatsWriter.flush();
 					objCatsWriter.close();
 					objCatsWriter = null;	// signal to open next one
 					debug(1,"Wrote "+nObjCatsDumpedToFile+" object categories to file: "+currFilename);
+				} else if( nObjCatsTillReport <= 0 ) {
+					debug(1,"Wrote "+nObjCatsDumpedToFile+" object categories to file: "+currFilename);
+					nObjCatsTillReport = nObjCatsReport;
 				}
 			}
 			if( objCatsWriter != null ) {
-				objCatsWriter.append(";");
-				objCatsWriter.newLine();
-				objCatsWriter.append("SHOW WARNINGS;");
+				if( fDumpAsSQLInsert ) {
+					objCatsWriter.append(";");
+					objCatsWriter.newLine();
+					objCatsWriter.append("SHOW WARNINGS;");
+				}
 				objCatsWriter.flush();
 				objCatsWriter.close();
 			}
@@ -1690,19 +1743,16 @@ public class MainApp {
 		return new Pair<ArrayList<String>,ArrayList<String>>(tokens, words);
 	}
 
-	private int categorizeAndDumpSQLForObjectAndFacet( int id, String source,
-			String facetName, DumpColumnConfigInfo colInfo, BufferedWriter writer,
-			boolean fFirst, boolean fWithNewlines ) {
-		ArrayList<TaxoNode> matchedCats = new ArrayList<TaxoNode>();
+	private void categorizeObjectForFacet( int id, String source,
+			String facetName, DumpColumnConfigInfo colInfo, HashMap<TaxoNode, Float> matchedCats ) {
 		// Tokensize the string for this column
 		Pair<ArrayList<String>,ArrayList<String>> tokenPair
 				= prepareSourceTokens( source, colInfo );
+		float reliability = colInfo.columnReliabilityForFacet(facetName);
 		// Now, we try to find a category in the hashMap for each token
 		for( String token:tokenPair.first ) {
 			TaxoNode node = facetMapHashTree.FindNodeByName(facetName,token.toLowerCase());
 			if( node == null )
-				continue;
-			if( matchedCats.contains(node))
 				continue;
 			debug(3, "Obj:"+id+" matched on token: ["+token+"] facet: "+facetName);
 			// We have a candidate here. Check for the exclusions
@@ -1721,42 +1771,49 @@ public class MainApp {
 				if( fCatExcluded )
 					continue;
 			}
-			// We have a TaxoNode match! Add to list if not already there.
-			matchedCats.add(node);
+			// We have a TaxoNode match! If not yet matched, or if this reliability
+			// is greater than any previous match, set the value in the matchedCats.
+			Float ret = matchedCats.get(node); // returns null if not in hashMap
+			float priorRel = (ret==null)?0:ret.floatValue();
+			if( reliability > priorRel )
+				matchedCats.put(node, reliability);
 		}
-		// TODO Need to consider the n-grams from the source words.
-		// Requires good exclusions, and we need to consider risks, e.g., for "red".
+	}
+
+	private int dumpSQLForObjCatsOnFacet( int id, HashMap<TaxoNode, Float> matchedCats,
+			BufferedWriter writer, boolean fFirst, boolean fWithNewlines, boolean fDumpAsSQLInsert ) {
 		// We just consider each match in turn, and get all the inferred nodes.
-		ArrayList<TaxoNode> inferredNodes = new ArrayList<TaxoNode>();
-		for( TaxoNode node:matchedCats ) {
-			node.AddInferredNodes(inferredNodes);
+		HashMap<TaxoNode, Float> inferredCats =  new HashMap<TaxoNode, Float>();
+		for( TaxoNode node:matchedCats.keySet() ) {
+			Float ret = matchedCats.get(node); // returns null if not in hashMap
+			float currRel = (ret==null)?0:ret.floatValue();
+			node.AddInferredNodes(inferredCats, currRel);
 		}
 
 		int nCatsMatched = 0;
-		// Now we have a list of the Nodes we matched, to dump
-		// As we go, we consider overlap with the matched list
-		// A matched node can also be an inferred node (this
-		// can happen when a parent and child both match in the categorization).
-		// For now, we filter these and emit them as inferred.
-		// Reset filterInferredMatches to emit as NOT inferred.
-		boolean filterInferredMatches = true;
-		float reliability = colInfo.columnReliabilityForFacet(facetName);
-		int iRel = Math.min(9,Math.round(10*reliability));
-		for( TaxoNode node:matchedCats ) { // Emit the match nodes.
-			// Check this match is also inferred byt something
-			if(inferredNodes.contains(node)) {
-				if(filterInferredMatches)
-					// Just skip this one - it will come out in the inferreds
-					continue;
-				else // put this out, and remove it from the inferreds
-					inferredNodes.remove(node);
+		// Now we have sets of the Nodes we matched and inferred, to dump
+		// We dump the mached cats that are not in the inferred map, then the inferred list
+		for( TaxoNode node:matchedCats.keySet() ) {
+			if( !inferredCats.containsKey(node)) {
+				Float ret = matchedCats.get(node); // returns null if not in hashMap
+				float reliability = (ret==null)?0:ret.floatValue();
+				int iRel = Math.min(9,Math.round(10*reliability));
+				if( fDumpAsSQLInsert )
+					nCatsMatched += dumpSQLInsertForObjectCat(id, node, false, iRel, writer, fFirst, fWithNewlines );
+				else
+					nCatsMatched += dumpSQLLoadFileForObjectCat(id, node, false, iRel, writer, fFirst, fWithNewlines );
+				fFirst = false;
 			}
-			nCatsMatched += dumpSQLForObjectCat(id, node, false, iRel, writer, fFirst, fWithNewlines );
-			fFirst = false;
 		}
 		// Now emit the (remaining) inferred nodes
-		for( TaxoNode node:inferredNodes ) {
-			nCatsMatched += dumpSQLForObjectCat(id, node, true, iRel, writer, fFirst, fWithNewlines );
+		for( TaxoNode node:inferredCats.keySet() ) {
+			Float ret = inferredCats.get(node); // returns null if not in hashMap
+			float reliability = (ret==null)?0:ret.floatValue();
+			int iRel = Math.min(9,Math.round(10*reliability));
+			if( fDumpAsSQLInsert )
+				nCatsMatched += dumpSQLInsertForObjectCat(id, node, true, iRel, writer, fFirst, fWithNewlines );
+			else
+				nCatsMatched += dumpSQLLoadFileForObjectCat(id, node, true, iRel, writer, fFirst, fWithNewlines );
 			fFirst = false;
 		}
 		return nCatsMatched;
