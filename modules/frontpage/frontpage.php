0aa61ecf240d4c6293f9a5a6510fd92917737295
geraldyu <geraldyu@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2007-05-01 05:07:41 +0000

    Frontpage: 1) featured objects have labels

--- modules/frontpage/frontpage.php
+++ modules/frontpage/frontpage.php
@@ -3,7 +3,7 @@
 require_once("../../libs/env.php");
 
 // Query DB
-$sql = "SELECT o.id, o.objnum, o.name, o.img_path FROM objects o, set_objs so 
+$sql = "SELECT o.id, o.objnum, o.name, o.img_path FROM objects o, set_objs so
 WHERE so.set_id=1 AND so.obj_id=o.id ORDER BY RAND() LIMIT 12";
 
 
@@ -22,16 +22,42 @@ if ( $res->numRows() < 1 ){
 
 $objects = array();
 
-while ($row = $res->fetchRow()) {
-	
-	$object = array(	'id' => $row['id'], 
-						'img_path' => $row['img_path'], 
-						'name' => $row['name'], 
-						'objnum' => $row['objnum']
-					);
-	
-	array_push($objects, $object);
-    
+//while ($row = $res->fetchRow()) {
+//
+//	$object = array(	'id' => $row['id'],
+//						'img_path' => $row['img_path'],
+//						'name' => $row['name'],
+//						'objnum' => $row['objnum']
+//					);
+//
+//	array_push($objects, $object);
+//
+//}
+
+$maxChars = 18;
+$ellipses = " ...";
+$eLen = strlen($ellipses);
+while( $row=$res->fetchRow()) {
+	$imageOutput = "<div class=\"front_example\">";
+	$pathToImg = $CFG->image_thumb . "/" . $row['img_path'];
+	$pathToDetails = $CFG->wwwroot . "/modules/browser/details.php?id=" . $row['id'];
+
+	$imageOutput .= "<a href=\"".$pathToDetails."\"><img src=\"".$pathToImg."\" alt=\"".$row['name']."\"";
+	$imageOutput .= " class=\"front_exampleThumbnail\" /></a>";
+
+	$text = $row['name'];
+
+	if (strlen($text) > $maxChars) {
+		$text = substr($text,0,$maxChars-$eLen);
+		$text = substr($text,0,strrpos($text,' '));
+		$text .= $ellipses;
+	}
+
+	$text = "<abbr title=\"".$row['name']."\">".$text."</abbr>";
+	$imageOutput .= "<div class=\"front_exampleLabel\"><a href=\"".$pathToDetails."\">".$text."</a></div>";
+	$imageOutput .= "</div>";
+
+	array_push($objects, $imageOutput);
 }
 
 // Free the result
