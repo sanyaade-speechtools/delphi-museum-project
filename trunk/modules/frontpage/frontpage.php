b4cdbcdec6e19cd9f825500667ca454442cc4caf
LudicrousResearcher@gmail.com <LudicrousResearcher@gmail.com@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2007-10-23 20:51:56 +0000

    Refactored the code to produce thumbnails. Added support for the image aspect ratio, and used this to properly lay out thumbnails.

--- trunk/modules/frontpage/frontpage.php
+++ trunk/modules/frontpage/frontpage.php
@@ -1,10 +1,39 @@
 <?
 
 require_once("../../libs/env.php");
+require_once("../common/imgthumb.php");
 
-// Query DB
-$sql = "SELECT o.id, o.objnum, o.name, o.img_path FROM objects o, set_objs so
+// Query DB for featured objects (set #1)
+$sql = "SELECT o.id, o.objnum, o.name, o.img_path, o.img_ar FROM objects o, set_objs so
 WHERE so.set_id=1 AND so.obj_id=o.id ORDER BY RAND() LIMIT 6";
+
+$res =& $db->query($sql);
+if (PEAR::isError($res)) {
+    die($res->getMessage());
+}
+
+// If nothing is found...  TODO this is not graceful enough
+if ( $res->numRows() < 1 ){
+	$t->display('objectNotFound.tpl');
+	die;
+}
+
+$objects = array();
+
+while( $row=$res->fetchRow()) {
+	array_push($objects, outputThumbnail( $row, "front_example" ));
+}
+
+// Free the result
+$res->free();
+
+// Assign vars to template
+$t->assign('objects', $objects);
+
+/*
+// Query DB for featured sets info
+$sql = "SELECT f.set_id, f.name, f.owner_name, f.img_path FROM featured_sets f
+ORDER BY f.order_num LIMIT 6";
 
 
 $res =& $db->query($sql);
@@ -12,38 +41,25 @@ if (PEAR::isError($res)) {
     die($res->getMessage());
 }
 
-// If nothing is found...
+// If nothing is found...  TODO this is not graceful enough
 if ( $res->numRows() < 1 ){
 	$t->display('objectNotFound.tpl');
 	die;
 }
 
 
+$sets = array();
 
-$objects = array();
-
-//while ($row = $res->fetchRow()) {
-//
-//	$object = array(	'id' => $row['id'],
-//						'img_path' => $row['img_path'],
-//						'name' => $row['name'],
-//						'objnum' => $row['objnum']
-//					);
-//
-//	array_push($objects, $object);
-//
-//}
-
-$maxChars = 18;
+$maxChars = 25;
 $ellipses = " ...";
 $eLen = strlen($ellipses);
 while( $row=$res->fetchRow()) {
-	$imageOutput = "<div class=\"front_example\">";
+	$imageOutput = "<div class=\"front_fset\">";
 	$pathToImg = $CFG->image_thumb . "/" . $row['img_path'];
-	$pathToDetails = $CFG->wwwroot . "/modules/browser/details.php?id=" . $row['id'];
+	$pathToSet = $CFG->wwwroot . "/modules/sets/viewset.php?sid=" . $row['set_id'];
 
-	$imageOutput .= "<a href=\"".$pathToDetails."\"><img src=\"".$pathToImg."\" alt=\"".$row['name']."\"";
-	$imageOutput .= " class=\"front_exampleThumbnail\" /></a>";
+	$imageOutput .= "<a href=\"".$pathToSet."\"><img src=\"".$pathToImg."\" alt=\"Set: ".$row['name']."\"";
+	$imageOutput .= " class=\"front_fset_Thumbnail\" /></a>";
 
 	$text = $row['name'];
 
@@ -54,23 +70,23 @@ while( $row=$res->fetchRow()) {
 	}
 
 	$text = "<abbr title=\"".$row['name']."\">".$text."</abbr>";
-	$imageOutput .= "<div class=\"front_exampleLabel\"><a href=\"".$pathToDetails."\">".$text."</a></div>";
+	$imageOutput .= "<div class=\"front_featuredSetName\"><a href=\"".$pathToSet."\">".$text."</a></div>";
+	$imageOutput .= "<div class=\"front_featuredSetCreator\">Created by <span>".$row['owner_name']."</span></div>";
 	$imageOutput .= "</div>";
 
-	array_push($objects, $imageOutput);
+	array_push($sets, $imageOutput);
 }
 
 // Free the result
 $res->free();
 
 // Assign vars to template
-$t->assign('objects', $objects);
+$t->assign('sets', $sets);
+ */
 
 // Display template
 $t->display('frontpage/frontpage.tpl');
 
 //Die here or additional PHP lines will be excecuted.
 die();
-
-
 ?>
