88d977430c7e00abf4db1f6def633b52fb0a2277
jonlesser <jonlesser@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2007-11-09 20:11:38 +0000

    fixing the error of my haste (an un commented comment)

--- trunk/libs/env.php
+++ trunk/libs/env.php
@@ -70,9 +70,8 @@ if( $login_state == DELPHI_LOGGED_IN || $login_state == DELPHI_REG_PENDING){
 		$t->assign('currentUser_isAuthStaff', TRUE );
 } else {
 	$t->assign('currentUser_loggedIn', FALSE);
-	
-	Get the name of the file being called
-	$scriptName = end (explode("/", $_SERVER['SCRIPT_NAME']) );
+	//	Get the name of the file being called
+	$scriptName = end(explode("/", $_SERVER['SCRIPT_NAME']) );
 	if ( $scriptName != "login.php" ) {
 		header( 'Location: ' . $CFG->wwwroot . '/modules/auth/login.php' );
 		die();
