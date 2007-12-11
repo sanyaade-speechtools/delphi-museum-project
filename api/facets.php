2522bd589b13d678ce862f2cdd5fd3e57cd35683
LudicrousResearcher <LudicrousResearcher@gmail.com>
2007-12-11 00:53:41 +0000

    Added support for param to specify which facet

--- api/facets.php
+++ api/facets.php
@@ -2,6 +2,12 @@
 require_once "apiSetup.php";
 require_once "../libs/ontology/ontoServices.php";
 
+if(empty($_GET['f'])) 
+	$fid = -1;
+else {
+	$facetname = $_GET['f'];
+	$fid = findFacetID( $facetname );
+}
 ?>
 <HTML>
 <BODY>
@@ -9,6 +15,8 @@ require_once "../libs/ontology/ontoServices.php";
 $facetinfo = getFacets();
 if( !$facetinfo || count($facetinfo) == 0 ) {
 	echo "<h2>Cannot get facets...</h2>";
+} else if( isset($facetname) && $fid < 0 ) {
+	echo "<h2>Bad facet name: ".$facetname."</h2>";
 } else {
 ?>
 	<h2>Facet info:</h2>
@@ -19,8 +27,9 @@ if( !$facetinfo || count($facetinfo) == 0 ) {
 	</TR>
 <?php
 	foreach( $facetinfo as $facet ) {
-		echo "<TR><TD><p>".$facet['name']."</p></TD>
-		      <TD><p>".$facet['id']."</p></TD></TR>";
+		if( $fid < 0 || $facet['id'] == $fid )
+			echo "<TR><TD><p>".$facet['name']."</p></TD>
+						<TD><p>".$facet['id']."</p></TD></TR>";
 	}
 }
 ?>
