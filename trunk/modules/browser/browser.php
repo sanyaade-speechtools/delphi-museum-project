6f1899e6c8ea7e169cdeb4a1c1c78776ad3aee95
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2008-02-05 01:17:45 +0000

    Added missing parameter to revised method getCategoriesInFacet().

--- trunk/modules/browser/browser.php
+++ trunk/modules/browser/browser.php
@@ -2,7 +2,8 @@
 require_once("../../libs/env.php");
 require_once("../../libs/ontology/ontoServices.php");
 
-$facetinfo = getCategoriesInFacet("__ALL", true, "HTML_UL" );
+$bigint = 999999;
+$facetinfo = getCategoriesInFacet("__ALL", true, "HTML_UL", $bigint );
 
 foreach( $facetinfo as $facet ) {
 	$t->assign("cats_".$facet['id'], $facet['items']);
