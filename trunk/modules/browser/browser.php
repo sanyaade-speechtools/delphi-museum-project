db1b69c1790115ca4fbb0cf961cdd55edcbd107e
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2008-03-03 07:14:51 +0000

    Revised to pass entire structure of results, so template can iterate over the facets.

--- trunk/modules/browser/browser.php
+++ trunk/modules/browser/browser.php
@@ -3,11 +3,10 @@ require_once("../../libs/env.php");
 require_once("../../libs/ontology/ontoServices.php");
 
 $bigint = 999999;
-$facetinfo = getCategoriesInFacet("__ALL", true, "HTML_UL", $bigint );
+$facetinfo = getCategoriesInFacet("__ALL", true, "HTML_UL_ATTR", "id", $bigint );
 
-foreach( $facetinfo as $facet ) {
-	$t->assign("cats_".$facet['id'], $facet['items']);
-}
+$t->assign("facets", $facetinfo);
+$t->assign("templateVarsJSON", json_encode($t->_tpl_vars));
 
 // Display template
 $t->display('browser.tpl');
