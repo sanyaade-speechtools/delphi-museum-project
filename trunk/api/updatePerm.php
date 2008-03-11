f7cc84cd2ef7160a7fc23d6e12ab0008719e38bc
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2008-03-11 05:18:16 +0000

    Added error checking and security measures to defend against nasty form submission.

--- trunk/api/updatePerm.php
+++ trunk/api/updatePerm.php
@@ -2,21 +2,33 @@
 //Bring in the user's config file
 require_once('apiSetup.php');
 
-	$badarg = false;
+	unset($errmsg);
 	if(empty($_POST['p']))
-		$badarg = true;
+		$errmsg = "Missing Permission name.";
 	else {
-		$permname = $_POST['p'];
-		$permdesc = $_POST['d'];
+		$permname = trim($_POST['p']);
+		if( strlen( $permname ) < 4 )
+			$errmsg = "Invalid permission name: [".$permname."]";
+		else if( preg_match( "/[^\w\s]/", $permname ))
+			$errmsg = "Invalid permission name (invalid chars): [".$permname."]";
+		else if(empty($_POST['d']))
+			$errmsg = "Missing permission description.";
+		else {
+			$permdesc = trim($_POST['d']);
+			if( strlen( $permdesc ) > 255 )
+				$errmsg = "Invalid permission description (too long);";
+			else if( preg_match( "/[^\w\-\s.:'()]/", $permdesc ))
+				$errmsg = "Invalid permission description (invalid chars): [".$permdesc."]";
+		}
 	}
-	if( $badarg ) {
+	if(!empty($errmsg)) {
 		header("HTTP/1.0 400 Bad Request");
-		echo "Bad Arg: p[".$permname."] d[".$permdesc."]";
-		echo "POST: ";
-		print_r( $_POST );
+		echo $errmsg;
 		exit();
 	}
-	$updateQ = "UPDATE permission set description='".$permdesc."' where name='".$permname."'";
+	$updateQ = "UPDATE permission set description='"
+		.mysql_real_escape_string($permdesc)."' where name='"
+		.mysql_real_escape_string($permname)."'";
 	$res =& $db->query($updateQ);
 	if (PEAR::isError($res)) {
 		header("HTTP/1.0 500 Internal Server Error\n"+$res->getMessage());
