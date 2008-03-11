47d509eb3bdeb1992ef0f33b6c4c57cd53c58cae
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2008-03-11 05:21:03 +0000

    Added error checking and security measures to defend against nasty form submission.

--- trunk/api/addRole.php
+++ trunk/api/addRole.php
@@ -2,22 +2,32 @@
 // set up env, DB
 require_once('apiSetup.php');
 
-	$badarg = false;
 	if(empty($_POST['r']))
-		$badarg = true;
+		$errmsg = "Missing role name.";
 	else {
-		$rolename = $_POST['r'];
-		$roledesc = $_POST['d'];
+		$rolename = trim($_POST['r']);
+		if( strlen( $rolename ) < 4 )
+			$errmsg = "Invalid role name: [".$rolename."]";
+		else if( preg_match( "/[^\w\s]/", $rolename ))
+			$errmsg = "Invalid role name (invalid chars): [".$rolename."]";
+		else if(empty($_POST['d']))
+			$errmsg = "Missing role description.";
+		else {
+			$roledesc = trim($_POST['d']);
+			if( strlen( $roledesc ) > 255 )
+				$errmsg = "Invalid role description (too long);";
+			else if( preg_match( "/[^\w\-\s.:'()]/", $roledesc ))
+				$errmsg = "Invalid role description (invalid chars): [".$roledesc."]";
+		}
 	}
-	if( $badarg ) {
+	if( $errmsg ) {
 		header("HTTP/1.0 400 Bad Request");
-		echo "Bad Arg: r[".$rolename."]";
-		echo "POST: ";
-		print_r( $_POST );
+		echo $errmsg;
 		exit();
 	}
 	$updateQ = "INSERT IGNORE INTO role(name, description, creation_time)"
-		." VALUES ('".$rolename."', '".$roledesc."', now())";
+		." VALUES ('".mysql_real_escape_string($rolename)."', '"
+		.mysql_real_escape_string($roledesc)."', now())";
 	$res =& $db->query($updateQ);
 	if (PEAR::isError($res)) {
 		header("HTTP/1.0 500 Internal Server Error\n"+$res->getMessage());
