1f5a7c127f68bc47b4de90a0d7aa99b1ff36999e
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2008-03-21 06:20:58 +0000

    Updated to query current user info and pre-fill form. Also fixed bug in which a submission response also shows the mail form.

--- modules/about/contact.php
+++ modules/about/contact.php
@@ -30,12 +30,29 @@ if(isset($_POST['submit'])){
 
 		if(sendDelphiMail($nameTo, $emailTo, $subj, $plaintextmsg, $htmlmsg, $emailFrom, $nameFrom)){
 			$t->display('contactSent.tpl');
+			die();
 		} else {
 			$t->assign('heading', "Failed to send message");
 			$t->assign('message', "<p>We were unable to deliver your message.</p> <p>You can <a href='".$CFG->wwwroot."/modules/about/contact.php'>try again</a> or send us an email directly at <a href='mailto:$emailTo'>$emailTo</a>.</p>");
 			$t->display('error.tpl');
 			die();
 		}
+	}
+}
+if( isset($_SESSION['id']) ) {
+	$q = "select username, real_name, email from user where id=".$_SESSION['id'];
+	$res =& $db->query($q);
+	if (!(PEAR::isError($res))) {
+		if($row = $res->fetchRow()) {
+			if( isset( $row['real_name'] ))
+				$t->assign('name', $row['real_name']);
+			else if( isset( $row['username'] ))
+				$t->assign('name', $row['username']);
+			if( isset( $row['email'] ))
+				$t->assign('email', $row['email']);
+		}
+		// Free the result
+		$res->free();
 	}
 }
 
