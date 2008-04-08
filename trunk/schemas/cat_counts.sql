4533bd94c785d0900db646c8a25fa81bfe3685a6
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2008-04-08 19:20:28 +0000

    Removed (non-portable) print suppression suffixes. No functional changes.

--- trunk/schemas/cat_counts.sql
+++ trunk/schemas/cat_counts.sql
@@ -2,7 +2,7 @@
 -- Table structure for table category counts
 -- The when field is in the timezone of the museum.
 --
-DROP TABLE IF EXISTS `cat_counts` \p;
+DROP TABLE IF EXISTS `cat_counts`;
 SHOW WARNINGS;
 CREATE TABLE `cat_counts` (
   `id` bigint(20) NOT NULL,
@@ -12,24 +12,24 @@ SHOW WARNINGS;
 
 -- Calculate counts overall and put in temp table
 INSERT INTO cat_counts( id, count )
-SELECT cat_id, count(*) FROM obj_cats group by cat_id \p;
+SELECT cat_id, count(*) FROM obj_cats group by cat_id;
 
 -- Add counts to categories table
 UPDATE categories, cat_counts SET categories.n_matches=cat_counts.count
-WHERE categories.id=cat_counts.id \p;
+WHERE categories.id=cat_counts.id;
 
 -- Clear temp table to start over
-TRUNCATE TABLE cat_counts \p;
+TRUNCATE TABLE cat_counts;
 
 -- Calculate counts with images and put in temp table
 INSERT INTO cat_counts( id, count )
 SELECT oc.cat_id, count(*) FROM obj_cats oc, objects o 
 where oc.obj_id=o.id and NOT o.img_path IS NULL 
-group by oc.cat_id \p;
+group by oc.cat_id;
 
 -- Add counts with images to categories table
 UPDATE categories, cat_counts SET categories.n_matches_w_img=cat_counts.count
-WHERE categories.id=cat_counts.id \p;
+WHERE categories.id=cat_counts.id;
 
 -- And clean up after ourselves
-DROP TABLE cat_counts \p;
+DROP TABLE cat_counts;
