c3ff0ce218ebfb5c945edd62db9f609150365e77
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2008-08-01 05:24:31 +0000

    Added new method getCategoryIDsForKwds() to support better search from the simple search box (and eventually, search within the ontology). Added comments on many routines.

--- libs/ontology/ontoServices.php
+++ libs/ontology/ontoServices.php
@@ -2,7 +2,10 @@
 session_cache_limiter("public");		// enable caching of this page
 
 require_once("Facet.inc");
+require_once("onto_utils.php");
 
+// Utility function to ensure that we have initialized the array of facets
+// in use in this ontology.
 function checkAndLoadFacets() {
 	global $facets;
 	global $db;
@@ -17,6 +20,9 @@ function checkAndLoadFacets() {
 	}
 }
 
+// Returns an array of structured info about the facets in the ontology
+// Each item in the indexed (not associative) array has the following fields:
+// "id", "name", "description" and "notes".
 function getFacets() {
 	global $facets;
 	checkAndLoadFacets();
@@ -31,6 +37,8 @@ function getFacets() {
 	return $retVal;
 }
 
+// Searches for a facet with a given name.
+// Returns facet id if found, or -1.
 function findFacetID( $fname /* :String */ ) {
 	global $facets;
 	checkAndLoadFacets();
@@ -43,6 +51,9 @@ function findFacetID( $fname /* :String */ ) {
 	return -1;
 }
 
+// Returns an associative array with two values:
+// "allObjs" => the count of all objects in the system
+// "withImgs" => the count of all objects that have at least one image
 function getNumObjs() {
 	global $db;
  	$allCountsResults =& $db->query("SELECT n_objs_total, n_objs_w_imgs FROM DBInfo LIMIT 1");
@@ -52,11 +63,43 @@ function getNumObjs() {
 		return false;
 }
 
+// Gets all the categories in the named facet, down to the specified depth
+// To get info on all facets, pass "__ALL" as the facet name.
+// $retType controls what form the returned information takes:
+//  "PHP":
+//     Returns an array (of length 1 for a single facet) of facet info structures: 
+//      "id", "name", "description" and "notes" contain basic info and
+//      "items" has a nested array of category information structures:
+//      "id", "name", "count" (the number of objects associated to the category)
+//       and "children" has the nested array of children categories.
+//  "JSON":
+//     Not Yet Implemented - will produce the PHP structure and then
+//      convert this to a JSON form and return it.
+//  "HTML_UL":
+//     Returns a string that contains HTML for the information, formatted as
+//      a nest unordered list (UL tags).
+//  "HTML_UL_ATAG":
+//     Returns a string that contains HTML for the information, formatted as
+//      a nest unordered list (UL tags), but with anchor tags on each category name.
+//      $HTparam specifies the base of the hyperlink, to which the id is appended.
+//  "HTML_UL_ATTR":
+//     Returns a string that contains HTML for the information, formatted as
+//      a nest unordered list (UL tags), and with id info in an attribute.
+//      $HTparam specifies the name of the attribute
+//  "HTML_UL_OC":
+//     Returns a string that contains HTML for the information, formatted as
+//      a nest unordered list (UL tags), and with onclick handler
+//      $HTparam specifies the name of the event handler
+//
+// If $countsWithImages is true, then all counts are for objects with images,
+// Else, the counts are for total objects.
+// TODO need to allow getting both counts at once.
+// $depth specifies the maximum depth to traverse the ontology.
 function getCategoriesInFacet(
 						$facetname,					//:String
 						$countsWithImages,	//:boolean
 						$retType, 					//:String 
-						$HTparam,							//:String
+						$HTparam,						//:String
 						$depth ) {					//:int
 	global $facets;
 	global $db;
@@ -132,6 +175,8 @@ function getCategoriesInFacet(
 }
 
 
+// Gets all the categories associated with the passed object id.
+// the rest of the parameters function as for getCategoriesInFacet()
 function getCategoriesForObject(
 						$forObjID,					//:int ID of object in DB
 						$facetname,					//:String 1 facet name or "__ALL"
@@ -211,6 +256,167 @@ function getCategoriesForObject(
 		}
 	}
 	return $retVal;
+}
+
+// Searches for categories that are tied to the passed keyword
+// by name or synonym.
+// Returns an object (array) with two array properties: 
+//   'cats' and 'kwds'. These are the found category ids, and
+//   all remaining keywords, respectively.
+//   
+// Will accept partial name matches, but prefers a full name.
+// For categories in a given facet, if one is an ascendant of another,
+// this will ignore the descendant.
+// Ideally, we would favor the pure match categories over the partials,
+// requiring that we pass back an array of pairs that indicate the weight.
+// I would set the weight as (# words in kwd)/(# words in category hook).
+// The passed Keyword may be multiple words, including quotation delimiters.
+// We need to consider things like "wood mask" as two keywords, but recognize
+// "San Francisco" as one. We'll have to be careful with this, since "San" will
+// match a lot of other locations. If the full string (or some n-gram substring)
+// matches in a given facet, that should preclude looking for matches on the words in
+// the n-gram (but not the other words, as in "west african wood masks" which should
+// match a location, a material and a use.
+// Select as OR and then group by obj id summing the number
+// of categories that were matched and ordering on that. Give text-only matches a "1".
+// BUT: If get exact match, then add as a category with an AND, not an OR. 
+// E.g., west african wood masks should be an AND of three categories.
+// Once we do this, we should promote the kwdCats to params so we do not repeat.
+function getCategoryIDsForKwds(
+						$kwdStr,
+						$countsWithImages ) {	//:boolean
+	//global $facets;
+	global $db;
+	$countCol = $countsWithImages ? "n_matches_w_img" : "n_matches";
+	$retval = array( 'cats' => array(), 'kwds' => array(), 'msg' => null, 'query' => null );
+
+	// Split on the quotes, then spaces to get token/words
+	$tokens = splitKwdString($kwdStr);
+
+	// Try longest n-grams first, and if get categories, drop all those words.
+	// This means the n-grams must have the string, the start index and length.
+	// If just order n-grams by length, then can proceed through list.
+	// Must also filter later n-grams contained by a matched n-gram, but see these
+	// easily by finding any n-gram with start-index from matched_start_index down
+	// to (matched_start_index - matched_length).
+	// For looping to work, must either be while(more) based or have markers to ignore.
+	// If put words into list as unigrams, then have unified loop.
+	// When done with loop, should have some set of categories.
+	// Now have to find ascendant/descendant relations and prune lower ones.
+	// Last, should we consider facets in this? Not sure; for now, no.
+
+	// Double check error condition - no params in.	
+	$nTokens = count($tokens);
+	if( $nTokens <= 0 ) {
+		$retval['msg'] = 'No tokens, so just returning nothing!';
+		return $retval;
+	}
+	
+	$ngrams = array();
+	// if only one word, build an ngram item from the one token
+	if( $nTokens == 1 ) {
+		$ngrams[] = array( 'len'   => 1,
+											 'start' => 0,
+											 'ngram' => strtolower($tokens[0]) );
+		$nNgrams = 1;
+	} else {
+		$ngrams = buildNGramsFromTokens($tokens);
+		$nNgrams = count( $ngrams );
+		if( $nNgrams <= 0 ) {
+			// Should complain somehow
+			$retval['kwds'] = $tokens;
+			$retval['msg'] = 'Could not create nGrams from tokens???';
+			return $retval;
+		}
+	}
+
+	$tqCatsForKwds = "select c.id cid, c.parent_id pid, c.facet_id fid,"
+										." LOWER(hk.token) token, CHAR_LENGTH(hk.token) as tlen, MAX(c.".$countCol
+										.") count from categories c, hooks hk where c.id=hk.cat_id"
+										." AND (";
+	// Should we put in the like matches as well? We can match against the tokens
+	// and figure out which are proper matches and which not.
+	// Not now - TODO?
+	for( $i = 0; $i < $nNgrams; $i++ ) {
+		if( $i > 0 )
+			$tqCatsForKwds .= " OR ";
+		$tqCatsForKwds .= "hk.token='".$ngrams[$i]['ngram']."'";
+	}
+	// We sort by token length to pull out longest n-grams.
+	// We use a secondary sort on count, so if we get multiple matches, we
+	// choose the category with the most associated objects.
+	$tqCatsForKwds .= ") GROUP BY token ORDER BY tlen desc";
+	// Only for debug!!!
+	$retval['query'] = $tqCatsForKwds;
+ 	$catsresult =& $db->query($tqCatsForKwds);
+	if (PEAR::isError($catsresult)) {
+		error_log( "getCategoryIDsForKwds() Query error: ".$tqCatsForKwds->getMessage());
+		error_log( "getCategoryIDsForKwds() Query : ".$tqCatsForKwds);
+		// Fall back to just returning the keywords as input.
+		$retval['kwds'] = $tokens;
+		$retval['msg'] = "getCategoryIDsForKwds() Query error: ".$tqCatsForKwds->getMessage();
+		return $retval;
+	}
+
+	$catsFound = array();
+	$retval['msg'] .= "Query returned ".$catsresult->numRows()." rows...";
+	while( $row=$catsresult->fetchRow() ) {
+		// We have to make sure that the token is still in the list we're considering.
+		// If we have "West African" and match the full token, we do not want to also
+		// match the token "African".
+		$iMatch = -1;
+		for( $i = 0; $i < $nNgrams; $i++ ) {
+			if( !strcmp( $ngrams[$i]['ngram'], $row['token']) ) {
+				$iMatch = $i;
+				break;
+			}
+		}
+		if( $iMatch >= 0 ) {
+			$retval['msg'] .= "<br />nGram match for row with token:".$row['token']." cat:".$row['cid'];
+			// We found a match. Add the category to the cats list
+			$catsFound[]= $row['cid']; 
+			// Now remove this and all overlapping ngrams from the list
+			$newNGrams = array();
+			$matchStart = $ngrams[$iMatch]['start'];
+			$matchEnd = $matchStart + $ngrams[$iMatch]['len']-1;
+			// Let's trim the tokens for this nGram from the tokens list
+			for( $i = $matchStart; $i <= $matchEnd; $i++ )
+				unset($tokens[$i]);
+			if( count($tokens) == 0 ) {
+				$retval['msg'] .= "<br />Matched all tokens to cats";
+				break;	// We're done
+			}
+
+			for( $i = 0; $i < $nNgrams; $i++ ) {
+				// If this is the one we matched, skip it
+				if( $i == $iMatch )
+					continue;
+				// OR if curr start is within the range of the matched one, skip it
+				$currStart = $ngrams[$i]['start'];
+				if(($currStart >= $matchStart) && ($currStart <= $matchEnd))
+					continue;
+				// OR if curr end is within the range of the matched one
+				$currEnd = $currStart + $ngrams[$i]['len']-1;
+				if(($currEnd >= $matchStart) && ($currEnd <= $matchEnd))
+					continue;
+				// If we reach here, the current nGram does not overlap with the matched one
+				$newNGrams[] = $ngrams[$i];
+			}
+			$ngrams = $newNGrams;
+			$nNgrams = count($newNGrams);
+			// If we've covered all the n-grams, then there's no point considering
+			// more of the query results.
+			if( $nNgrams <= 0 )
+				break;
+		} else {
+			$retval['msg'] .= "<br />No nGram match for row with token:"
+											.$row['token']." cat:".$row['cid'];
+		}
+	}
+	$retval['cats'] = $catsFound;
+	$retval['kwds'] = array_values($tokens);	// collapse all the unset values and re-index
+
+	return $retval;
 }
 
 $_DELPHI_PAGE_SIZE = 40;	// TODO move to config.php
