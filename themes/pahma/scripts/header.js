29e69a55423ea878166df8f4ede078d4b094ab66
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2008-08-21 07:23:39 +0000

    Added support for searching on object numbers - uses simple pattern from PAHMA for now. Need to configure this at some point.

--- themes/pahma/scripts/header.js
+++ themes/pahma/scripts/header.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
 	$("label.overlabel").overlabel();
+	
+	// Add Search box override to check for objnums
+	$("#navBarSearchBoxForm").submit(function(){
+		// See if it looks like an obj num
+		kwdVal = $('#navSearchBoxInput').val();
+		objNumRegex = new RegExp("^\\d{1,2}-\\d*");
+		if (objNumRegex.test(kwdVal)) {
+			// call object details page with alternate param to get details by objNum
+			newURL = templateVarsJSON['wwwroot'] + "/modules/browser/details.php?onum=" + kwdVal;
+			window.location = newURL;
+			// Return false so normal page handling does not kick in
+			return false;
+		} else {
+			// Let the normal page handling kick in
+			return true;
+		}
+	});
+	
 });
