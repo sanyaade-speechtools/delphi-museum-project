236026039ccc89ecd1fce9c3f266751c390e0052
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2008-08-21 08:46:06 +0000

    Fixed a bug in search box override for objnums on pages that did not have JSON variables set.

--- trunk/themes/pahma/scripts/header.js
+++ trunk/themes/pahma/scripts/header.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
 	$("label.overlabel").overlabel();
 	
-	// Add Search box override to check for objnums
 	$("#navBarSearchBoxForm").submit(function(){
-		// See if it looks like an obj num
 		kwdVal = $('#navSearchBoxInput').val();
+		// See if it looks like an obj num
 		objNumRegex = new RegExp("^\\d{1,2}-\\d*");
 		if (objNumRegex.test(kwdVal)) {
-			// call object details page with alternate param to get details by objNum
-			newURL = templateVarsJSON['wwwroot'] + "/modules/browser/details.php?onum=" + kwdVal;
+			newURL = wwwroot + "/modules/browser/details.php?onum=" + kwdVal;
 			window.location = newURL;
 			// Return false so normal page handling does not kick in
 			return false;
 		} else {
-			// Let the normal page handling kick in
+			// Let normal page handling kick in
 			return true;
 		}
 	});
