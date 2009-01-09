2239fde9fa2036d6e6aa8043c5699d5e6cd24590
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2009-01-09 16:42:56 +0000

    Added support to categorize all facets at once in one pass. Crucial to support complex inferencing (coming next). Fixed bug that did not categorize the last line in the file. Changed the way exclusions are handled to default to considering the entire text for an object, and not just the local token. The simpler mechanism misses too many cases. May consider providing control when configure the exclusion, but it will be hard for most people to set this reasonably, and so this more conservative default is likely fine. Restructured the way we filter inferences to better support extension and to provide a reliability boost for an inferred concept when we filter it as a matched one. Changed the prepareToken mechanism to fold to lower to reduce string processing elsewhere.

--- trunk/tools/DelphiImporter/src/museum/delphi/Categorizer.java
+++ trunk/tools/DelphiImporter/src/museum/delphi/Categorizer.java
@@ -28,20 +28,26 @@ public class Categorizer {
 	}
 
 	/**
-	 * @param facetName facet to categorize
+	 * Processes the object metadata to produce concept associations
+	 * @param metaDataReader the object metadata source
+	 * @param columnNames the configured column names in the metadata source
+	 * @param facetMapHashTree the concept ontology, by facet
+	 * @param facetName the name of the facet, or null to do all at once
+	 * @param fDumpAsSQLInsert set TRUE for an insert statement, FALSE for a loadfile
+	 * @param dbName the name of the DB to be used.
 	 */
 	public static void categorizeForFacet(MetaDataReader metaDataReader,
 			String columnNames[],
 			DoubleHashTree facetMapHashTree, String facetName,
-			boolean fDumpAsSQLInsert, String dbName ) {
+			String basefilename, boolean fDumpAsSQLInsert, String dbName ) {
     	String filename = null;
-    	String basefilename = null;
     	String extension = ".sql";
     	String currFilename = null;
     	int iOutputFile;
     	int nObjCatsOutMax = Integer.MAX_VALUE;
     	int nObjCatsReport = 100000;
     	int nObjCatsTillReport;
+    	// If need to build files under a size limit (e.g., 5 MB):
     	// Each line in the output file is about 20 chars max.
     	// We want a max of 5 MB files, so we can take up to
     	// 5000000 / 20 = 250000.
@@ -61,17 +67,13 @@ public class Categorizer {
 				throw new RuntimeException("categorizeForFacet: DumpColConfig info not yet built!");
 			colDumpInfo = DumpColumnConfigInfo.getAllColumnInfo(columnNames);
 			int nCols = columnNames.length;
+			// Since we run through lots of lines, cache the skip columns info for speed
 			boolean[] skipCol = new boolean[nCols];
 			for( int iCol=0; iCol<nCols; iCol++ ) {
-				skipCol[iCol] = ( colDumpInfo[iCol].columnReliabilityForFacet(facetName) == 0);
+				skipCol[iCol] = !((facetName==null)?
+								 colDumpInfo[iCol].columnMinedForAnyFacet()
+								:colDumpInfo[iCol].columnMinedForFacet(facetName));
 			}
-			filename = metaDataReader.getFileName();
-			// Find last slash or backslash (to allow for windows paths), to get basepath
-	    	int iSlash = Math.max(filename.lastIndexOf('/'), filename.lastIndexOf('\\'));
-	    	//int iDot = filename.lastIndexOf('.');
-	    	if( iSlash<=0 )
-	    		throw new RuntimeException("categorizeForFacet: odd input filename:\n"+filename);
-	    	basefilename = filename.substring(0, iSlash+1)+"obj_cats."+facetName+"_";
 			BufferedWriter objCatsWriter = null;
 	    	iOutputFile = 0;
 			ArrayList<String> nextLine;
@@ -81,46 +83,55 @@ public class Categorizer {
 			ArrayList<String> lastStrings = new ArrayList<String>();
 			HashMap<TaxoNode, Float> matchedCats = new HashMap<TaxoNode, Float>();
 			nObjCatsTillReport = nObjCatsReport;
-			// TODO - BUG: does not categorize last object in file.
-			while((nextLine = metaDataReader.getNextLineAsColumns()) != null ) {
-				if(nextLine.get(objIDCol).length() == 0) {
-					debug(2,"Skipping line with empty id" );
-					continue;
-				}
-				int id = Integer.parseInt(nextLine.get(objIDCol));
-				if( lastIDVal < 0 ) {
-					lastStrings.addAll(nextLine);
-					lastIDVal = id;
-					continue;
-				}
-				if( id == lastIDVal ) {
-					// Consider merging the two lines, but only for the columns we care about.
-					// Separate them by '|' chars to ensure we tokenize in next step
-					// Note that we always skip col 0, the ID column
-					for(int i=1; i<nCols; i++ ) {
-						if(skipCol[i])				// Do not bother with misc cols
-							continue;
-						String nextToken = nextLine.get(i);
-						if(nextToken.length()==0)	// If new token is empty ("") skip it
-							continue;
-						String last = lastStrings.get(i);
-						if(last.length()==0)		// If old token is empty (""), just set
-							lastStrings.set(i, nextLine.get(i));
-						else if(nextLine.get(i).equalsIgnoreCase(last)
-								|| last.contains(nextLine.get(i)))
-							continue;
-						else
-							lastStrings.set(i, last+"|"+nextLine.get(i));
-						debug(3,"Combining "+columnNames[i]+ " for id: "+id+
-											" ["+lastStrings.get(i)+"]");
+			boolean fMoreLines = true;
+			while( fMoreLines ) {
+				int id = -1;
+				if((nextLine = metaDataReader.getNextLineAsColumns()) == null) {
+					fMoreLines = false;
+					// Handle case of empty file.
+					if( lastStrings.size() == 0 )
+						break;
+				} else {
+					// Look at new line and consider merging with previous
+					if(nextLine.get(objIDCol).length() == 0) {
+						debug(2,"Skipping line with empty id" );
+						continue;
 					}
-					continue;
+					id = Integer.parseInt(nextLine.get(objIDCol));
+					if( lastIDVal < 0 ) {
+						lastStrings.addAll(nextLine);
+						lastIDVal = id;
+						continue;
+					}
+					if( id == lastIDVal ) {
+						// Consider merging the two lines, but only for the columns we care about.
+						// Separate them by '|' chars to ensure we tokenize in next step
+						// Note that we always skip col 0, the ID column
+						for(int i=1; i<nCols; i++ ) {
+							if(skipCol[i])				// Do not bother with misc cols
+								continue;
+							String nextToken = nextLine.get(i);
+							if(nextToken.length()==0)	// If new token is empty ("") skip it
+								continue;
+							String last = lastStrings.get(i);
+							if(last.length()==0)		// If old token is empty (""), just set
+								lastStrings.set(i, nextLine.get(i));
+							else if(nextLine.get(i).equalsIgnoreCase(last)
+									|| last.contains(nextLine.get(i)))
+								continue;
+							else
+								lastStrings.set(i, last+"|"+nextLine.get(i));
+							debug(4,"Combining "+columnNames[i]+ " for id: "+id+
+												" ["+lastStrings.get(i)+"]");
+						}
+						continue;
+					}
 				}
 				// Have a complete line. Check for validity
 				String objNumStr = lastStrings.get(objNumCol);
 				if(!DumpColumnConfigInfo.objNumIsValid(objNumStr)) {
 					// Skip lines with no object number.
-					debug(1,"CategorizeForFacet: Discarding line for id (bad objNum): "+id );
+					debug(1,"CategorizeForFacet: Discarding line for id (bad objNum): "+lastIDVal );
 					nObjsSkippedTotal++;
 				} else { //  Process a valid line
 					if( objCatsWriter == null ) {
@@ -147,39 +158,23 @@ public class Categorizer {
 						String source = lastStrings.get(i);
 						if( source.length() <= 1 )
 							continue;
-						categorizeObjectForFacet( lastIDVal, source,
+						if( facetName != null )
+							categorizeObjectForFacet( lastIDVal, source, lastStrings,
 								facetName, colDumpInfo[i], facetMapHashTree, matchedCats );
+						else
+							categorizeObjectForAllFacets( lastIDVal, source, lastStrings,
+									colDumpInfo[i], facetMapHashTree, matchedCats );
 					}
-					int nCatsDumped = SQLUtils.writeObjCatsOnFacetSQL( lastIDVal, matchedCats,
-											objCatsWriter, fFirst, fWithNewlines, fDumpAsSQLInsert );
+					HashMap<TaxoNode, Float> inferredCats = addSimpleInferences(matchedCats);
+					filterMatchedInferences( matchedCats, inferredCats);
+					int nCatsDumped = SQLUtils.writeObjCatsSQL( lastIDVal,
+											matchedCats, inferredCats, objCatsWriter,
+											fFirst, fWithNewlines, fDumpAsSQLInsert );
+					matchedCats.clear();
 					if( nCatsDumped > 0) {
 						nObjCatsDumpedTotal += nCatsDumped;
 						nObjCatsDumpedToFile += nCatsDumped;
 						nObjCatsTillReport -= nCatsDumped;
-						fFirst = false;
-					}
-					matchedCats.clear();
-					// BUG? We already ran against the line - why do this again?!?!
-					if( nObjCatsDumpedTotal >= nObjCatsOutMax ) {
-						if(lastStrings.size() > 0) {
-							for(int i=1; i<nCols; i++ ) {
-								if(skipCol[i])				// Do not bother with misc cols
-									continue;
-								String source = lastStrings.get(i);
-								if( source.length() <= 1 )
-									continue;
-								categorizeObjectForFacet( lastIDVal, source,
-										facetName, colDumpInfo[i],
-										facetMapHashTree, matchedCats );
-							}
-						}
-						break;
-					}
-					nCatsDumped = SQLUtils.writeObjCatsOnFacetSQL( lastIDVal, matchedCats,
-							objCatsWriter, fFirst, fWithNewlines, fDumpAsSQLInsert );
-					if( nCatsDumped > 0) {
-						// Gratuitous since have already decided to quit
-						// nObjsProcessedTotal += nCatsFound;
 						fFirst = false;
 					}
 					if( nObjCatsDumpedToFile >= nObjsCatsPerDumpFile ) {
@@ -193,6 +188,8 @@ public class Categorizer {
 						debug(1,"Wrote "+nObjCatsDumpedToFile+" object categories to file: "+currFilename);
 						nObjCatsTillReport = nObjCatsReport;
 					}
+					if( nObjCatsDumpedTotal >= nObjCatsOutMax )
+						break;
 				}
 				lastIDVal = id;
 				lastStrings = nextLine;
@@ -206,7 +203,8 @@ public class Categorizer {
 				objCatsWriter.flush();
 				objCatsWriter.close();
 			}
-			debug(1,"Wrote "+nObjCatsDumpedTotal+" total object categories for facet: "+facetName);
+			debug(1,"Wrote "+nObjCatsDumpedTotal+" total object categories"
+					+ ((facetName==null)?"":(" for facet: "+facetName)));
 		} catch( IOException e ) {
 			String tmp = "Categorizer.categorizeForFacet: Could not create or other problem writing:\n  "
 				+((filename==null)?"null":filename)
@@ -223,7 +221,7 @@ public class Categorizer {
 		}
 	}
 
-	private static void categorizeObjectForFacet( int id, String source,
+	private static void categorizeObjectForFacet( int id, String source, ArrayList<String> allStrings,
 			String facetName, DumpColumnConfigInfo colInfo,
 			DoubleHashTree facetMapHashTree, HashMap<TaxoNode, Float> matchedCats ) {
 		// Tokenize the string for this column
@@ -236,25 +234,11 @@ public class Categorizer {
 			TaxoNode node = facetMapHashTree.FindNodeByHook(facetName,token);
 			if( node == null )
 				continue;
-			debug(3, "Obj:"+id+" matched on token: ["+token+"] facet: "+facetName);
+			debug(3, "Obj:"+id+" matched concept: ["+
+					facetName+":"+node.displayName+"] on token: "+token);
 			// We have a candidate here. Check for the exclusions
-			if( node.exclset != null ) {
-				boolean fCatExcluded = false;
-				// TODO once we have collation based checking, remove this.
-				String tokenL = token.toLowerCase();
-				for( String excl:node.exclset ) {
-					// We'll have to consider whether and when to search
-					// the entire string for the exclusion. May need to put
-					// a flag on the exclusion terms.
-					if( tokenL.indexOf(excl) >= 0 ) {
-						fCatExcluded = true;
-						debug(3, "Obj:"+id+" match on token excluded on: ["+excl+"]");
-						break;
-					}
-				}
-				if( fCatExcluded )
-					continue;
-			}
+			if( lineContainsExclusions( node.exclset, allStrings ))
+				continue;
 			// We have a TaxoNode match! Update matches with this node
 			updateMatches( matchedCats, reliability, node);
 			// Now, consider any pending implied nodes as well.
@@ -263,6 +247,61 @@ public class Categorizer {
 					updateMatches( matchedCats, reliability, node.impliedNodes.get(i));
 				}
 		}
+	}
+
+	private static void categorizeObjectForAllFacets( int id, String source, ArrayList<String> allStrings,
+			DumpColumnConfigInfo colInfo,
+			DoubleHashTree facetMapHashTree, HashMap<TaxoNode, Float> matchedCats ) {
+		// Tokenize the string for this column. First list is tokens, second is words.
+		Pair<ArrayList<String>,ArrayList<String>> tokenPair
+				= prepareSourceTokens( source, colInfo );
+		// Now, we try to find a category in each facet hashMap for each token
+		for( String token:tokenPair.first ) {
+			// Loop over the facets to mine list. Note that list specifies order.
+			// As we find a token, we remove it from further consideration so we
+			// get some auto-exclusion (should we configure this?)
+			for( DumpColumnConfigInfo.MineInfoForColumn mineInfo:colInfo.facetsToMine ) {
+				// Test where id is 317239 - how do we match FUR?
+				TaxoNode node = facetMapHashTree.FindNodeByHook(mineInfo.facetName,token);
+				if( node == null )
+					continue;
+				debug(3, "Obj:"+id+" matched concept: ["+
+						mineInfo.facetName+":"+node.displayName+"] on token: "+token);
+				// We have a candidate here. Check for the exclusions
+				// TODO - shouldn't the exclusion be checked in the entire string?
+				// If we only check the current token, then we'll only catch exclusions
+				// that match hooks!
+				if( lineContainsExclusions( node.exclset, allStrings )) {
+					debug(3, "Obj:"+id+" excluding matched concept: ["+
+							mineInfo.facetName+":"+node.displayName+"]");
+					continue;
+				}
+				// We have a TaxoNode match! Update matches with this node
+				updateMatches( matchedCats, mineInfo.reliability, node);
+				// Now, consider any pending implied nodes as well.
+				// TODO Verify that this should never happen, and remove this code.
+				if(( node.impliedNodesPending != null ) && ( node.impliedNodesPending.size() > 0 ))
+					for( int i=0; i<node.impliedNodesPending.size(); i++ ) {
+						updateMatches( matchedCats, mineInfo.reliability, node.impliedNodes.get(i));
+					}
+				// Since we got a match, we may stop considering the other facets for this token.
+				if( mineInfo.filterTokenOnMatch )
+					break;
+			}
+		}
+	}
+
+	private static boolean lineContainsExclusions(ArrayList<String> exclSet, ArrayList<String> line) {
+		if( exclSet != null && line != null ) {
+			for( String str:line) {
+				String strLower = str.toLowerCase();
+				for( String excl:exclSet ) {
+					if( strLower.contains(excl) )
+						return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	private static void updateMatches(
@@ -277,9 +316,62 @@ public class Categorizer {
 			matchedCats.put(node, baseReliability);
 	}
 
+	/**
+	 * Adds all the simply inferred concepts for the set of matched concepts.
+	 * Also removes all matched concepts that were otherwise inferred, so we
+	 * do not duplicate the entries in the index.
+	 * @param matchedCats the set of matched concepts
+	 * @return the set of inferred concepts
+	 */
+	protected static HashMap<TaxoNode, Float> addSimpleInferences(
+						HashMap<TaxoNode, Float> matchedCats) {
+		HashMap<TaxoNode, Float> inferredCats = new HashMap<TaxoNode, Float>();
+		// We just consider each match in turn, and get all the inferred nodes.
+		for( TaxoNode node:matchedCats.keySet() ) {
+			Float ret = matchedCats.get(node); // returns null if not in hashMap
+			float currRel = (ret==null)?0:ret.floatValue();
+			node.AddInferredNodes(inferredCats, currRel);
+		}
+		return inferredCats;
+	}
+
+	/**
+	 * Removes all matched concepts that were otherwise inferred, so we
+	 * do not duplicate the entries in the index.
+	 * @param matchedCats the set of matched concepts
+	 * @param inferredCats the set of inferred concepts
+	 * @return the set of inferred concepts
+	 */
+	protected static void filterMatchedInferences( HashMap<TaxoNode, Float> matchedCats,
+						HashMap<TaxoNode, Float> inferredCats) {
+		// Remove all the matchedCats that were otherwise inferred.
+		// Before we do that, we adjust the reliability of the inferred node.
+		ArrayList<TaxoNode> toRemove = null;
+		for( TaxoNode node:matchedCats.keySet() ) {
+			if( inferredCats.containsKey(node)) {
+				Float ret = matchedCats.get(node);	// Should not return null, but be safe
+				float matchRel = (ret==null)?0:ret.floatValue();
+				ret = inferredCats.get(node);		// Should not return null, but be safe
+				float infRel = (ret==null)?0:ret.floatValue();
+				// We will boost reliability given match and inference
+				float boostedRel = (float)(1.0 - ((1.0-matchRel)*(1.0-infRel)));
+				inferredCats.put(node, boostedRel);
+				// And add to list of nodes to remove from Matched Cats
+				if( toRemove == null)
+					toRemove = new ArrayList<TaxoNode>();
+				toRemove.add(node);
+			}
+		}
+		// Now remove the nodes from the matchedCats set.
+		if( toRemove != null )
+			for( TaxoNode node:toRemove )
+				matchedCats.remove(node);
+	}
+
 	//
 	/**
-	 * Tokenizes an input string according to column info rules
+	 * Tokenizes an input string according to column info rules.
+	 * Also maps all tokens and words to lower case.
 	 * @param source input String
 	 * @param colInfo columnInfo for this string
 	 * @return Pair of Lists: first is tokens, second is words
@@ -289,7 +381,7 @@ public class Categorizer {
 		ArrayList<String> tokens = new ArrayList<String>();
 		ArrayList<String> words = new ArrayList<String>();
 		// First we apply reduction. This cleans up certain oddities in the source
-		String reducedSource = source;
+		String reducedSource = source.toLowerCase();
 		for(Pair<String, String> reduce : colInfo.reduceRules) {
 			reducedSource = reducedSource.replaceAll(reduce.first, reduce.second);
 		}
