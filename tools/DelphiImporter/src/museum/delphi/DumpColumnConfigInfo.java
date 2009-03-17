bf8c6f0d3328deed1fdb993f7a53eaab68b5a3dd
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2009-03-17 00:06:39 +0000

    Cleaned up imports.

--- tools/DelphiImporter/src/museum/delphi/DumpColumnConfigInfo.java
+++ tools/DelphiImporter/src/museum/delphi/DumpColumnConfigInfo.java
@@ -1,19 +1,11 @@
 package museum.delphi;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.Node;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
 
 // TODO This class does not present a clean model: the static class members are ugly.
 // It should be rewritten to support a static class factory that takes the config
