397b293f8de660d20655a1c1e12d6e5906f720a7
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2009-03-17 00:04:56 +0000

    Commented out notes.

--- tools/DelphiImporter/src/museum/delphi/DBMetaDataReader.java
+++ tools/DelphiImporter/src/museum/delphi/DBMetaDataReader.java
@@ -6,7 +6,7 @@ import org.w3c.dom.Element;
 
 public class DBMetaDataReader {
 
-	*** how to store all the info - this is generic, not protocol specific.
+	/*** how to store all the info - this is generic, not protocol specific.
 	Need to model each source as a class, etc.
 	Column is a local class of the source.
 	JoinColumn subclasses this? May be no need, despite overlap.
@@ -15,14 +15,15 @@ public class DBMetaDataReader {
 		RowFilter info (only one)
 		ArrayList<something> that specifies replace rules
 		ArrayList<something> that specifies elide rules (combine with above?)
+	*/
 	private static int _debugLevel = 1;
 
-	protected static void debug( int level, String str ){
+	protected static void debug( int level, String str ) {
 		if( level <= _debugLevel )
 			StringUtils.outputDebugStr(str);
 	}
 
-	protected static void debugTrace( int level, Exception e ){
+	protected static void debugTrace( int level, Exception e ) {
 		if( level <= _debugLevel )
 			StringUtils.outputExceptionTrace(e);
 	}
