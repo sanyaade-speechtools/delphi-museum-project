dfcec7ac1e4211d8e69f8d111e5c0bb904156621
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2009-03-27 20:34:01 +0000

    Added override annotations - no functional change.

--- trunk/tools/DelphiImporter/src/museum/delphi/Pair.java
+++ trunk/tools/DelphiImporter/src/museum/delphi/Pair.java
@@ -24,7 +24,8 @@ public class Pair<F,S> {
     second = pSecond;
   }
 
-  public boolean equals(Object o) {
+  @Override
+public boolean equals(Object o) {
     if (this == o) return true;
     if (!(o instanceof Pair)) return false;
 
@@ -36,14 +37,16 @@ public class Pair<F,S> {
     return true;
   }
 
-  public int hashCode() {
+  @Override
+public int hashCode() {
     int result;
     result = (first != null ? first.hashCode() : 0);
     result = 29 * result + (second != null ? second.hashCode() : 0);
     return result;
   }
 
-  public String toString() {
+  @Override
+public String toString() {
     return "(" + getFirst() + ", " + getSecond() + ")";
   }
 
