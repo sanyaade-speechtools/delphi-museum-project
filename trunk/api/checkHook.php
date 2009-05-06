6363f9a486c47727884cabf556fde27ecf5fb0bf
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2009-05-06 00:08:25 +0000

    Improved layout of reporting, separated full matches from partial. Fixed bug where partial matches exactly matched lowLimit, but "more" link was still shown.

--- trunk/api/checkHook.php
+++ trunk/api/checkHook.php
@@ -2,6 +2,15 @@
 require_once "apiSetup.php";
 ?>
 <HTML>
+<style>
+tr.hdr { background-color:#DDD; font-family:Verdana, Arial; font-size:0.9em; }
+td.facet { text-align:right; }
+td.cat { text-align:left; font-weight:bold; }
+td.matches { text-align:left; font-style:italic; }
+td.hspace { height:5px; }
+span.term { background-color:#ffff66; }
+span.fullterm { background-color:#ff6666; }
+</style>
 <BODY>
 <?php
 	$lowLimit = 10;
@@ -12,41 +21,78 @@ require_once "apiSetup.php";
 			$catlimit = trim($_GET['catlimit']);
 		else
 			$catlimit = $lowLimit;
-		$results =& $db->query(
-				"SELECT f.display_name facet, c.display_name category,"
-			 ." SUBSTRING_INDEX(GROUP_CONCAT(h.token SEPARATOR '|'), '|', 5) matches"
+		$qString1 = 
+				"SELECT f.display_name, c.display_name, h.token "
 			 ." FROM hooks h JOIN categories c on h.cat_id=c.id JOIN facets f on c.facet_id=f.id"
-			 ."	where h.token like '%".$term."%' GROUP BY h.cat_id LIMIT ".$catlimit);
+			 ."	where h.token='".$term."' LIMIT ".$catlimit;
+		$qString2 = 
+				"SELECT SQL_CALC_FOUND_ROWS f.display_name, c.display_name,"
+			 ." SUBSTRING_INDEX(GROUP_CONCAT(h.token SEPARATOR '|'), '|', 5)"
+			 ." FROM hooks h JOIN categories c on h.cat_id=c.id JOIN facets f on c.facet_id=f.id"
+			 ."	where (h.token like '%".$term."%') AND (h.token!='".$term."') GROUP BY h.cat_id LIMIT ".$catlimit;
+		$results =& $db->query($qString1);
 		if (PEAR::isError($results)) {
 				echo "Error in query: ".$results->getMessage();
+				echo "Query: ".$qString;
 		} else {
-			$nrows = 0;
+			$nrows1 = 0;
 			while ($row = $results->fetchRow(MDB2_FETCHMODE_ORDERED)) {
-				if($nrows==0) {
+				if($nrows1==0) {
 					echo '<h3>Results of check against term: "'.$_GET['term'].'":</h3>'
-							.'<p style="text-decoration: underline;"><em>Facet.Category :   matches</p><p>';
+						  .'<table cellspacing="0" cellpadding="0">'
+							.'<tr class="hdr"><td class="facet">Facet:</td><td>Category</td><td>&nbsp;&nbsp;&nbsp;matches</td></tr><tr><td class="hspace" colspan="3"></td></tr>';
 				}
 				$fname = $row[0];
 				$cname = $row[1];
-				$count;
-				$matches = str_replace("|", ", ", $row[2], $count);
-				echo '<b>'.$fname.".".$cname.'</b> :  '.$matches;
-				if($count>=4)
-					echo ' ...';
-				echo '<br />';
-				$nrows++;
+				$match = str_replace($term, '<span class="fullterm">&nbsp;'.$term.'&nbsp;</span>', $row[2]);
+				echo '<tr><td class="facet">'.$fname.':</td><td class="cat">'.$cname
+							.'&nbsp;&nbsp;</td><td class="matches">'.$match.'</td></tr>';
+				$nrows1++;
 			}
-			if($nrows==0) {
-					echo '<h3>No matches found for term: "'.$_GET['term'].'"</h3>';
-			} else if($nrows==$lowLimit) {
-				$pageURL = 'http://'.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].'&catlimit='.$highLimit;
-				echo '...<a href="'.$pageURL.'">more</a>...<br /></p>';
-			} else if($nrows==$highLimit) {
-				echo '...(results truncated at '.$highLimit.')...<br /></p>';
+			$results->free();
+			// Now get the partial match results
+			$results =& $db->query($qString2);
+			if (PEAR::isError($results)) {
+					echo "Error in query: ".$results->getMessage();
+					echo "Query: ".$qString;
 			} else {
-				echo '</em></p>';
+				$nrows2 = 0;
+				while ($row = $results->fetchRow(MDB2_FETCHMODE_ORDERED)) {
+					if($nrows1==0&&$nrows2==0) {
+						echo '<h3>Results of check against term: "'.$_GET['term'].'":</h3>'
+								.'<table cellspacing="0" cellpadding="0">'
+								.'<tr class="hdr"><td class="facet">Facet:</td><td>Category</td><td>&nbsp;&nbsp;&nbsp;matches</td></tr><tr><td class="hspace" colspan="3"></td></tr>';
+					}
+					$fname = $row[0];
+					$cname = $row[1];
+					$match = str_replace($term, '<span class="term">'.$term.'</span>', $row[2]);
+					echo '<tr><td class="facet">'.$fname.':</td><td class="cat">'.$cname
+								.'&nbsp;&nbsp;</td><td class="matches">'.$match.'</td></tr>';
+					$nrows2++;
+				}
+			}
+			$results->free();
+			$results =& $db->query("SELECT FOUND_ROWS()");
+			if(!PEAR::isError($results)&&($row = $results->fetchRow(MDB2_FETCHMODE_ORDERED))) {
+				$nrowsTotal = $row[0];
+			} else {
+				$nrowsTotal = -1;
+			}
+
+			if($nrows1==0&&$nrows2==0) {
+				echo '<h3>No matches found for term: "'.$_GET['term'].'"</h3>';
+			} else {
+				if($nrows2==$lowLimit && $nrowsTotal>$lowLimit) {
+					$pageURL = 'http://'.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].'&catlimit='.$highLimit;
+					echo '<tr><td></td><td colspan="2">...<a href="'.$pageURL.'">more</a>...</td></tr>';
+				} else if($nrows2>=$highLimit && $nrowsTotal>$highLimit) {
+					echo '<tr><td></td><td colspan="2">...(results truncated at '.$highLimit.')...</td></tr>';
+				}
+				echo '</table>';
 			}
 		echo '<hr>';
+		//echo "<p>Query1: ".$qString1.'</p>';
+		//echo "<p>Query2: ".$qString2.'</p>';
 		}
 	}
 ?>
