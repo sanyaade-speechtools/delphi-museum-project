c97617ceaf439a02135a69b2032493b2afbc0c35
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2009-06-05 05:36:43 +0000

    Updated term to be text, since some are long. Dropped the simple key on term, since we have the fulltext.

--- trunk/schemas/termStats.sql
+++ trunk/schemas/termStats.sql
@@ -7,9 +7,8 @@
 -- does not exceed 255 or we need to change from VARCHAR to text.
 DROP TABLE IF EXISTS `termStats`;
 CREATE TABLE `termStats` (
-  `term`     VARCHAR(255) NOT NULL,
+  `term`     text NOT NULL,
   `count`    INT(10) NOT NULL,
-  INDEX `termStats_index` (`term`),
 	-- Enable full text (keyword) search on the name and description
   FULLTEXT KEY `terms_fulltext_index` (`term`)
 )ENGINE=MyIsam;
