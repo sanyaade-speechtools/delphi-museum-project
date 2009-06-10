9e67120dca26657701776acd526a45c3068e9a14
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2009-06-10 16:17:32 +0000

    Fixed typo in updating category(concept) counts.

--- tools/DelphiImporter/src/museum/delphi/WebAppDB.java
+++ tools/DelphiImporter/src/museum/delphi/WebAppDB.java
@@ -197,7 +197,7 @@ public class WebAppDB {
 			"INSERT INTO obj_cats_wimgs (obj_id, cat_id, inferred, reliability)"
 			+ " SELECT oc.obj_id, oc.cat_id, oc.inferred, oc.reliability"
 			+ " FROM obj_cats oc, objects o WHERE oc.obj_id=o.id AND NOT o.img_path IS NULL";
-		retVal = uploadData( "obj_cats", loadObjCatsWImgsStmt, null) && retVal;
+		retVal = uploadData( "obj_cats_wimgs", loadObjCatsWImgsStmt, null) && retVal;
 		return retVal;
 	}
 
