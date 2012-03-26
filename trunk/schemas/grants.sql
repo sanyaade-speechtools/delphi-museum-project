25e39b07ef90a7325736f43b2bc9707b37115ed7
LudicrousResearcher <LudicrousResearcher@3f9da2c7-0530-0410-8862-5d2c5c102a8f>
2012-03-26 18:11:59 +0000

    NOJIRA - made grants more generic. Moved local stuff to site-specific files.

--- trunk/schemas/grants.sql
+++ trunk/schemas/grants.sql
@@ -4,24 +4,15 @@
 
 -- You must set the password for your installation!
 
-GRANT SELECT ON pahma_dev.* TO 'pahma'@'webfarm%.berkeley.edu' IDENTIFIED BY 'password';
+GRANT SELECT ON pahma_dev.* TO 'pahma'@'yourhost' IDENTIFIED BY 'password';
 
-GRANT INSERT,UPDATE,DELETE ON pahma_dev.user
-TO 'pahma'@'webfarm%.berkeley.edu' IDENTIFIED BY 'password';
-GRANT INSERT,UPDATE,DELETE ON pahma_dev.permission 
-TO 'pahma'@'webfarm%.berkeley.edu' IDENTIFIED BY 'password';
-GRANT INSERT,UPDATE,DELETE ON pahma_dev.role 
-TO 'pahma'@'webfarm%.berkeley.edu' IDENTIFIED BY 'password';
-GRANT INSERT,UPDATE,DELETE ON pahma_dev.user_roles
-TO 'pahma'@'webfarm%.berkeley.edu' IDENTIFIED BY 'password';
-GRANT INSERT,UPDATE,DELETE ON pahma_dev.role_perms 
-TO 'pahma'@'webfarm%.berkeley.edu' IDENTIFIED BY 'password';
-GRANT INSERT,UPDATE,DELETE ON pahma_dev.sets 
-TO 'pahma'@'webfarm%.berkeley.edu' IDENTIFIED BY 'password';
-GRANT INSERT,UPDATE,DELETE ON pahma_dev.set_objs 
-TO 'pahma'@'webfarm%.berkeley.edu' IDENTIFIED BY 'password';
-GRANT INSERT,UPDATE,DELETE ON pahma_dev.tags 
-TO 'pahma'@'webfarm%.berkeley.edu' IDENTIFIED BY 'password';
-GRANT INSERT,UPDATE,DELETE ON pahma_dev.tag_user_object
-TO 'pahma'@'webfarm%.berkeley.edu' IDENTIFIED BY 'password';
+GRANT INSERT,UPDATE,DELETE ON pahma_dev.user TO 'pahma'@'yourhost'
+GRANT INSERT,UPDATE,DELETE ON pahma_dev.permission TO 'pahma'@'yourhost' 
+GRANT INSERT,UPDATE,DELETE ON pahma_dev.role TO 'pahma'@'yourhost' 
+GRANT INSERT,UPDATE,DELETE ON pahma_dev.user_roles TO 'pahma'@'yourhost' 
+GRANT INSERT,UPDATE,DELETE ON pahma_dev.role_perms TO 'pahma'@'yourhost' 
+GRANT INSERT,UPDATE,DELETE ON pahma_dev.sets TO 'pahma'@'yourhost' 
+GRANT INSERT,UPDATE,DELETE ON pahma_dev.set_objs TO 'pahma'@'yourhost' 
+GRANT INSERT,UPDATE,DELETE ON pahma_dev.tags TO 'pahma'@'yourhost' 
+GRANT INSERT,UPDATE,DELETE ON pahma_dev.tag_user_object TO 'pahma'@'yourhost' 
 
